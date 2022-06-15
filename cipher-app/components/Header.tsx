87ed9a30330ba551a47d16e326846a874e3ef9aa
Sandip Thapa <sandipthapa@cagtu.com>
2022-06-15 14:22:51 +0545

    Project synced as that of NX

---
+++ cipher-app/components/Header.tsx
@@ -0,0 +1,104 @@
+import { faBars, faTelescope, faListCheck, faObjectsColumn, faLocationDot, faAngleDown } from "@fortawesome/pro-regular-svg-icons";
+import { faUserHelmetSafety } from "@fortawesome/pro-thin-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import {NextRouter,useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Container, Navbar } from "react-bootstrap";
+
+const Header = () => {
+    const router = useRouter();
+    const handleMenuActive = (path:string, router: NextRouter) => {
+        const defaultClass = "nav-item";
+        const activeClass = defaultClass + " nav-item--active";
+        return router.pathname == path ? activeClass : defaultClass;
+    };
+
+    const [stickyClass, setStickyClass] = useState('relative');
+    useEffect(() => {
+        window.addEventListener('scroll', stickNavbar);
+    
+        return () => {
+          window.removeEventListener('scroll', stickNavbar);
+        };
+      }, []);
+
+      const stickNavbar = () => {
+        if (window !== undefined) {
+          const windowHeight = window.scrollY;
+          windowHeight > 85 ? setStickyClass('sticky') : setStickyClass('normal');
+        }
+      };
+
+    return (
+        <>
+        {/* Site Upper Header Start */}
+        <header
+                id="site-header"
+                className={`site-header ${stickyClass}`}
+            >
+                <Container className="">
+                    <Navbar expand="lg" className="header-navigation">
+                            <nav className="navbar-nav ms-lg-auto">
+                            <li className={handleMenuActive("/how-it-works", router)}>
+                                    <Link href="/">
+                                        <a className="nav-link">
+                                            <FontAwesomeIcon icon={faTelescope} className="svg-icon d-none d-sm-inline-block"/>
+                                            Explore Services
+                                        </a>
+                                    </Link>
+                                </li>
+                                <li className={handleMenuActive(
+                                        "/features",
+                                        router
+                                    )}>
+                                    <Link href="/resources">
+                                        <a className="nav-link">
+                                            <FontAwesomeIcon icon={faListCheck} className="svg-icon d-none d-sm-inline-block"/>
+                                            Find Tasks
+                                        </a>
+                                    </Link>
+                                </li>
+                                <li className={handleMenuActive(
+                                        "/features",
+                                        router
+                                    )}>
+                                    <Link href="/resources">
+                                        <a className="nav-link">
+                                            <FontAwesomeIcon icon={faUserHelmetSafety} className="svg-icon d-none d-sm-inline-block"/>
+                                            Find Merchants
+                                        </a>
+                                    </Link>
+                                </li>
+                                <li className={handleMenuActive(
+                                        "/features",
+                                        router
+                                    )}>
+                                    <Link href="/resources">
+                                        <a className="nav-link d-none d-md-inline-block">
+                                            <FontAwesomeIcon icon={faObjectsColumn} className="svg-icon"/>
+                                            Categories
+                                            <FontAwesomeIcon icon={faAngleDown} className="faAngleDown-svg-icon"/>
+                                        </a>
+                                    </Link>
+                                </li>
+                            </nav>
+                        
+                        <Link href="#!">
+                            <a className="btn location-btn d-none d-md-inline-block">
+                                Nepal
+                                <FontAwesomeIcon icon={faLocationDot} className="svg-icon"/>
+                            </a>
+                        </Link>
+                        {/* <Button type="button" className="mega-menu-toggler">
+                            <DragHandle className="svg-icon" />
+                        </Button> */}
+                    </Navbar>
+                </Container>
+            </header>
+        {/* Site Upper Header End */}
+        </>
+    )
+}
+
+export default Header;
---
+++ cipher-app/types/metaData.ts
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export interface MetaDataProps{
+    title?:string;
+    description?:string;
+    ogImage?:string;
+    ogUrl?:string;
+    keywords?:string;
+    children: ReactNode;
+}
