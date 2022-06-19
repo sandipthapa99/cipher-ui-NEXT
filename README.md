e580b82e35d536d7a46d9626611aa03d48890f6b
Sandip Thapa <sandipthapa@cagtu.com>
2022-06-19 19:18:13 +0545

    Folder restructured

--- README.md
+++ README.md
@@ -1,37 +1,34 @@
-### This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) for project Cipher.
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Prerequisites
-You will need [Node.js](https://nodejs.org) version 8.0 or greater installed on your system.
+## Getting Started
 
-## Setup
-Get the code by either cloning this repository using git
+First, run the development server:
 
+```bash
+npm run dev
+# or
+yarn dev
 ```
-https://gitlab.com/sandipthapa/cipher.git
-```
-... or [downloading source code](https://gitlab.com/sandipthapa/cipher/-/archive/main/cipher-main.zip) as a zip archive.
 
-## Running locally in development mode
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-To get started, just clone the repository and run `yarn install && yarn dev`:
+You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
 
-    git clone https://gitlab.com/sandipthapa/cipher.git
-    yarn install
-    yarn dev
+[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
 
+The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
 
-## Building and deploying in production
+## Learn More
 
-If you wanted to run this site in production, you should install modules then build the site with `Yarn build` and run it with `yarn start`:
+To learn more about Next.js, take a look at the following resources:
 
-    git clone https://gitlab.com/sandipthapa/cipher.git
-    yarn install
-    yarn build
-    yarn start
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-## Building and Running in Docker environment
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
 
-    sudo docker-compose build
-    sudo docker-compose up
+## Deploy on Vercel
 
-### Now open [http://localhost:3004](http://localhost:3004) with your browser to see the result.
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ pages/index.tsx
@@ -0,0 +1,173 @@
+import SelectInputField from '@components/common/SelectInputField'
+import Layout from '@components/Layout'
+import { faChevronDown, faSearch } from '@fortawesome/pro-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Formik } from 'formik'
+import type { NextPage } from 'next'
+import Image from 'next/image'
+import { NextRouter } from 'next/router'
+import { Button, Col, Container, Form, FormControl, Row } from 'react-bootstrap'
+import HomeSearchSchema from 'utils/formValidation/homeSearchValidation'
+import { HomeSearchdata } from 'utils/homeSearchData'
+import {
+    myOptions
+} from "utils/options";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      {/* Site Main Banner Start */}
+      <section id="main-banner" className="site-main-banner ">
+                <span className="shape-blob d-none d-md-inline-block"></span>
+                <Container fluid="xl" className="px-4">
+                    <div className="site-main-banner--content d-flex align-items-center justify-content-center text-center">
+                      {/* Hero Text Start Here */}
+                        <div className="site-main-banner--content__inner">
+                            <h1 className="site-main-banner--content__inner--title">
+                            Catering To Your Requirements
+                            </h1>
+                            <p>
+                            Discover the world of services & get paid for your time
+                            </p>
+                        </div>
+                        {/* Hero Text End Here */}
+                    </div>
+
+                    <div className="site-main-banner--search-bar">
+                        <Formik
+                        initialValues={HomeSearchdata}
+                        validationSchema={HomeSearchSchema}
+                        onSubmit={async(values, actions) => {console.log(values)}}>
+                            <div className="search_box">
+                                <div className="dropdown-wrapper">
+                                    <div className="dropdown">
+                                        <SelectInputField
+                                            name="experience"
+                                            placeHolder="All"
+                                            options={myOptions}
+                                            fieldRequired
+                                        />
+                                        <FontAwesomeIcon icon={faChevronDown} className="svg-icon"/>
+                                    </div>
+                                </div>
+                                <div className="search_field">
+                                    <input type="text" className="input" placeholder="Find your services"/>
+                            </div>
+                                    <Button className='search-btn'>
+                                        <FontAwesomeIcon icon={faSearch} className="icon"/>
+                                    </Button>
+                                </div>
+                        </Formik>
+                    </div>
+
+
+                    <Row className="gx-5">
+                        <Col xs={6} sm={4} lg={2}>
+                            <div className="card-block">
+                                <div className="card-block__image-block">
+                                    <figure className="thumbnail-icon">
+                                        <Image
+                                            src="/heroImages/img1.png"
+                                            alt="assist-img1"
+                                            height={48}
+                                            width={48}
+                                        />
+                                    </figure>
+                                </div>
+                                <p>Health & Care</p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={4} lg={2}>
+                            <div className="card-block">
+                                <div className="card-block__image-block">
+                                    <figure className="thumbnail-icon">
+                                        <Image
+                                            src="/heroImages/img2.png"
+                                            alt="assist-img2"
+                                            height={48}
+                                            width={48}
+                                        />
+                                    </figure>
+                                </div>
+                                <p>Household</p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={4} lg={2}>
+                            <div className="card-block">
+                                <div className="card-block__image-block">
+                                    <figure className="thumbnail-icon">
+                                        <Image
+                                            src="/heroImages/img3.png"
+                                            alt="assist-img3"
+                                            height={48}
+                                            width={48}
+                                        />
+                                    </figure>
+                                </div>
+                                <p>Beauty</p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={4} lg={2}>
+                            <div className="card-block">
+                                <div className="card-block__image-block">
+                                    <figure className="thumbnail-icon">
+                                        <Image
+                                            src="/heroImages/img4.png"
+                                            alt="assist-img4"
+                                            height={48}
+                                            width={48}
+                                        />
+                                    </figure>
+                                </div>
+                                <p>Courier Services</p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={4} lg={2}>
+                            <div className="card-block">
+                                <div className="card-block__image-block">
+                                    <figure className="thumbnail-icon">
+                                        <Image
+                                            src="/heroImages/img5.png"
+                                            alt="assist-img5"
+                                            height={48}
+                                            width={48}
+                                        />
+                                    </figure>
+                                </div>
+                                <p>Maintenance</p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={4} lg={2}>
+                            <div className="card-block">
+                                <div className="card-block__image-block">
+                                    <figure className="thumbnail-icon">
+                                        <Image
+                                            src="/heroImages/img6.png"
+                                            alt="assist-img6"
+                                            height={48}
+                                            width={48}
+                                        />
+                                    </figure>
+                                </div>
+                                <p>Fitness</p>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    
+                </Container>
+      </section>
+      {/* Site Main Banner End */}
+
+      {/* Services near you section start */}
+      {/* <section id="services-near-you" className="services-near-you">
+        <Container>
+        <h2 className="heading-title">Services near you</h2>
+        </Container>
+      </section> */}
+      {/* Services near you section end */}
+    </Layout>
+  )
+}
+
+export default Home
