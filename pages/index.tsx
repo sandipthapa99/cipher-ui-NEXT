4d722609233da7f20243902b2d1d7b1e15242f3f
Sandip Thapa <sandipthapa@cagtu.com>
2022-06-21 22:56:15 +0545

    Find opportunities section done

--- pages/index.tsx
+++ pages/index.tsx
@@ -283,6 +283,97 @@ const Home: NextPage = () => {
         </Container>
       </section>
       {/* Top Rated Merchant Section End */}
+
+      {/* Find oppurtunities section start */}
+      <section id="find-hire" className="find-hire">
+        <Container fluid="xl">
+            <h1 className="section-main-title">Find Oppurtunities</h1>
+            <h2 className="section-sub-title">Finding job can never be this easy</h2>
+            <Row>
+                <Col md={4} className="d-flex align-items-stretch">
+                <div className="find-hire-card-block">
+                    <figure className="thumbnail-img">
+                        <Image
+                        src="/services/opp1.png"
+                        layout="fill"
+                        objectFit="cover"
+                        alt=""
+                        />
+                        
+                    </figure>
+                    <div className="card-content">
+                        <h2>Win new clients </h2>
+                        <p>
+                        Have a project that you need to finish ? Log in to CIPHER, type in the requirements, your budget, time constraints and post the same. Sit back, and relax while CIPHER finds a suitable tasker for you.
+                        </p>
+
+                        <Link href="">
+                            <a>
+                            Win new clients
+                                <FontAwesomeIcon icon={faAngleRight} className="svg-icon"/>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                </Col>
+                <Col md={4} className="d-flex align-items-stretch">
+                <div className="find-hire-card-block">
+                    <figure className="thumbnail-img">
+                        <Image
+                        src="/services/opp2.png"
+                        layout="fill"
+                        objectFit="cover"
+                        alt=""
+                        />
+                        
+                    </figure>
+                    <div className="card-content">
+                        <h2>Grow Your Business</h2>
+                        <p>
+                        Looking for some place to showcase your talent, and earn while you do it ? Well, look no more, login to CIPHER, and access all the tasks on the portal for you to choose from. 
+
+                        </p>
+
+                        <Link href="">
+                            <a>
+                            Grow your business
+                                <FontAwesomeIcon icon={faAngleRight} className="svg-icon"/>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                </Col>
+                <Col md={4} className="d-flex align-items-stretch">
+                <div className="find-hire-card-block">
+                    <figure className="thumbnail-img">
+                        <Image
+                        src="/services/opp3.png"
+                        layout="fill"
+                        objectFit="cover"
+                        alt=""
+                        />
+                        
+                    </figure>
+                    <div className="card-content">
+                        <h2>Get Help</h2>
+                        <p>
+                        Stuck somewhere on CIPHER, have questions ? Fret not, our hands on team is always ready to address your queries, and provide you with real time soultions.
+                        </p>
+
+                        <Link href="">
+                            <a>
+                                Get Help
+                                <FontAwesomeIcon icon={faAngleRight} className="svg-icon"/>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                </Col>
+            </Row>
+            
+        </Container>
+      </section>
+      {/* Find oppurtunities section end */}
     </Layout>
   )
 }
