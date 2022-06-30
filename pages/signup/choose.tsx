ccf2de34d97e22ed4649cd4ec341b55af2843cb0
Sandip Thapa <sandipthapa@cagtu.com>
2022-06-30 11:01:52 +0545

    State management of accout type choose during sign up

--- pages/signup/choose.tsx
+++ pages/signup/choose.tsx
@@ -2,9 +2,13 @@ import FormButton from "@components/common/FormButton";
 import OnBoardingLayout from "@components/OnBoardingLayout";
 import { Field, Form, Formik } from "formik";
 import Image from "next/image";
+import { useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import { isSubmittingClass } from "utils/helpers";
 
 const Choose = () => {
+    const [user, setUser] = useState<string>("client");
+
     return (
         <OnBoardingLayout
             topLeftText="Already have an account?"
@@ -26,49 +30,63 @@ const Choose = () => {
                         isSubmitting,
                     }) => (
                         <Form>
-                            <div className="radio-cards-wrapper">
-                                <label htmlFor="client">
-                                    <div className="img-wrapper">   
-                                        <figure>
+                            <Row className="radio-cards-wrapper">
+                                <Col md={6}>
+                                    <div className="choose-card-block" onClick={() => setUser("client")}>
+                                        <span className={`${user == "client" ? "radio radio-active" : "radio"}`}></span>
+                                        <figure className="thumbnail-img">
                                             <Image
                                                 src="/illustrations/signup-as-client.svg"
-                                                width={100}
                                                 height={100}
+                                                width={100}
+                                                objectFit="cover"
+                                                alt="client-image"
                                             />
                                         </figure>
+                                        <div className="card-content">
+                                            <div className="account-description">
+                                                <h2 className="account-title">I am client hiring for services</h2>
+                                                <p className="account-description">
+                                                    I am looking forward to hire professional individual and get my work done as quick as possible. I believe in paying good amount for satisfied task.
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
-                                    I am client hiring for services
-                                    <p>I am looking forward to hire professional individual and get my work done as quick as possible. I believe in paying good amount for satisfied task.</p>
-                                    <Field name="user" type="radio" id="client" value="client" />
-                                    <span aria-hidden="true" className="radioIcon"></span>
-                                    <span aria-hidden="true" className="focusIndicator"></span>
-                                </label>
+                                </Col>
+                                <Col md={6}>
+                                    <div className="choose-card-block" onClick={() => setUser("merchant")}>
+                                        <span className={`${user == "merchant" ? "radio radio-active" : "radio"}`}></span>
+                                        <figure className="thumbnail-img">
+                                            <Image
+                                                src="/illustrations/signup-as-tasker.svg"
+                                                height={80}
+                                                width={80}
+                                                objectFit="cover"
+                                                alt="merchant-image"
+                                            />
+                                        </figure>
+                                        <div className="card-content">
+                                            <div className="account-description">
+                                                <h2 className="account-title">I am merchant looking for tasks</h2>
+                                                <p className="account-description">
+                                                I am a hardworking person who seeks oppurtunity in every task focusing on uplifting my careers to next heights. I priotorize in completing my tasks.
+                                                </p>
+                                            </div>
+                                        </div>
 
-                                <label htmlFor="merchant">
-                                    <div className="img-wrapper">
-                                        <figure>
-                                            <Image
-                                                src="/illustrations/signup-as-client.svg"
-                                                width={100}
-                                                height={100}
-                                            />
-                                        </figure>
                                     </div>
-                                    I am merchant looking for tasks
-                                    <p>I am a hardworking person who seeks oppurtunity in every task focusing on uplifting my careers to next heights. I priotorize in completing my tasks..</p>
-                                    <Field name="user" type="radio" id="merchant" value="merchant" />
-                                    <span aria-hidden="true" className="radioIcon"></span>
-                                    <span aria-hidden="true" className="focusIndicator"></span>
-                                </label>
+                                </Col>
+                            </Row>
+                            <div className="continue-btn-wrapper">
+                                <FormButton
+                                    type="submit"
+                                    variant="primary"
+                                    name="Continue"
+                                    className="continue-btn"
+                                    isSubmitting={isSubmitting}
+                                    isSubmittingClass={isSubmittingClass(isSubmitting)}
+                                />
                             </div>
-                            <FormButton
-                                type="submit"
-                                variant="primary"
-                                name="Continue"
-                                className="login-btn"
-                                isSubmitting={isSubmitting}
-                                isSubmittingClass={isSubmittingClass(isSubmitting)}
-                            />
 
                         </Form>
                     )}
