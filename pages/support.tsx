f2377d0f8be525a80d2fa6678c72ea0e774403eb
Sandip <sandipthapa@cagtu.com>
2022-07-07 17:56:22 +0545

    Support initial commit second draft

--- pages/support.tsx
+++ pages/support.tsx
@@ -15,89 +15,89 @@ const Support = () => {
         <>
             <Layout title="Feedback | Cipher">
                 <Container>
-                <section className="site-feedback">
-                    <Row className="gx-5">
-                        <Col md={6} className="d-none d-md-flex">
-                            <figure className="thumbnail-img">
-                                <Image
-                                    src="/feedback.svg"
-                                    layout="fill"
-                                    objectFit="cover"
-                                    alt="merchant-image"
-                                />
-                            </figure>
+                    <section className="site-feedback">
+                        <Row className="gx-5">
+                            <Col md={6} className="d-none d-md-flex">
+                                <figure className="thumbnail-img">
+                                    <Image
+                                        src="/feedback.svg"
+                                        layout="fill"
+                                        objectFit="cover"
+                                        alt="merchant-image"
+                                    />
+                                </figure>
 
-                        </Col>
-                        <Col md={6}>
-                            <h1>Support Form</h1>
-                            <p>Any question or queries? Just write us a message</p>
-                            <Formik
-                                initialValues={FeedbackFormData}
-                                validationSchema={FeedbackFormSchema}
-                                onSubmit={async (values) => {
-                                    console.log(values)
-                                }}
-                            >
-                                {({
-                                    isSubmitting,
-                                    errors,
-                                    touched,
-                                }) => (
-                                    <Form>
-                                        <InputField
-                                            type="text"
-                                            name="fullName"
-                                            labelName="Your Name"
-                                            error={errors.fullName}
-                                            touch={touched.fullName}
-                                            placeHolder="Enter your full name"
-                                        />
-                                        <InputField
-                                            type="email"
-                                            name="email"
-                                            labelName="Email"
-                                            error={errors.email}
-                                            touch={touched.email}
-                                            placeHolder="Enter your email"
-                                        />
-                                        <InputField
-                                            type="phone"
-                                            name="phone"
-                                            labelName="Phone Number"
-                                            error={errors.email}
-                                            touch={touched.email}
-                                            placeHolder="Enter your email"
-                                        />
-                                        <SelectInputField
-                                            name="experience"
-                                            placeHolder="All"
-                                            labelName="Issue Type"
-                                            options={myOptions}
+                            </Col>
+                            <Col md={6}>
+                                <h1>Support Form</h1>
+                                <p>Any question or queries? Just write us a message</p>
+                                <Formik
+                                    initialValues={FeedbackFormData}
+                                    validationSchema={FeedbackFormSchema}
+                                    onSubmit={async (values) => {
+                                        console.log(values)
+                                    }}
+                                >
+                                    {({
+                                        isSubmitting,
+                                        errors,
+                                        touched,
+                                    }) => (
+                                        <Form>
+                                            <InputField
+                                                type="text"
+                                                name="fullName"
+                                                labelName="Your Name"
+                                                error={errors.fullName}
+                                                touch={touched.fullName}
+                                                placeHolder="Enter your full name"
+                                            />
+                                            <InputField
+                                                type="email"
+                                                name="email"
+                                                labelName="Email"
+                                                error={errors.email}
+                                                touch={touched.email}
+                                                placeHolder="Enter your email"
+                                            />
+                                            <InputField
+                                                type="text"
+                                                name="phone_number"
+                                                labelName="Phone Number"
+                                                touch={touched.phone_number}
+                                                error={errors.phone_number}
+                                                placeHolder="+00 420 420 4200"
+                                            />
+                                            <SelectInputField
+                                                name="experience"
+                                                placeHolder="All"
+                                                labelName="Issue Type"
+                                                options={myOptions}
                                             // fieldRequired
-                                        />
-                                        <InputField
-                                            name="message"
-                                            labelName="Message"
-                                            touch={touched.message}
-                                            error={errors.message}
-                                            placeHolder="Write your message here..."
-                                            as="textarea"
-                                        />
-                                        
-                                        <FormButton
-                                            type="submit"
-                                            variant="primary"
-                                            name="Send"
-                                            className="submit-btn"
-                                            isSubmitting={isSubmitting}
-                                            isSubmittingClass={isSubmittingClass(isSubmitting)}
-                                        />
-                                    </Form>
-                                )}
-                            </Formik>
-                        </Col>
-                    </Row>
-                </section>
+                                            />
+                                            <InputField
+                                                name="message"
+                                                labelName="Message"
+                                                touch={touched.message}
+                                                error={errors.message}
+                                                placeHolder="Write your message here..."
+                                                as="textarea"
+                                            />
+
+                                            <FormButton
+                                                type="submit"
+                                                variant="primary"
+                                                name="Send"
+                                                className="submit-btn"
+                                                isSubmitting={isSubmitting}
+                                                isSubmittingClass={isSubmittingClass(isSubmitting)}
+                                            />
+                                        </Form>
+                                    )}
+                                </Formik>
+                            </Col>
+                        </Row>
+                    </section>
                 </Container>
 
             </Layout>
