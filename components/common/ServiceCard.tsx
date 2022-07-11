79eca8b0a86b12797ed651f050e1adc8fb84101f
Sandip <sandipthapa@cagtu.com>
2022-07-11 10:41:35 +0545

    Services card made dynamic with offers

--- components/common/ServiceCard.tsx
+++ components/common/ServiceCard.tsx
@@ -14,36 +14,47 @@ const ServiceCard = ({
     serviceDescription,
     serviceRating,
     servicePrice,
-}:ServiceCardProps) => {
-    return(
+    hasOffer,
+    discountRate,
+    discountOn
+}: ServiceCardProps) => {
+    return (
         <div className="service-card-block">
-            <figure className="thumbnail-img">
-                <Image
-                    src={serviceImage}
-                    layout="fill"
-                    objectFit="cover"
-                    alt="servicecard-image"
-                />
-            </figure>
+            <div className="card-img">
+                <figure className="thumbnail-img">
+                    <Image
+                        src={serviceImage}
+                        layout="fill"
+                        objectFit="cover"
+                        alt="servicecard-image"
+                    />
+                </figure>
+                {hasOffer &&
+                    <div className="offer">
+                        <p className="discount-rate">{discountRate}% OFF</p>
+                        <p className="discount-on">{discountOn}</p>
+                    </div>
+                }
+            </div>
             <div className="card-content">
                 <h2 className="card-title">{serviceTitle}</h2>
                 <h3 className="card-subtitle"><span>{serviceProvider}</span> | {serviceProviderLocation}</h3>
                 <p className="card-description">
-                {`${serviceDescription.substring(0, 80)}...`} 
+                    {`${serviceDescription.substring(0, 80)}...`}
                 </p>
                 <div className="ratings-wrapper d-flex justify-content-between">
                     <p className='ratings d-flex align-items-center justify-content-center'>
-                        <FontAwesomeIcon icon={faStar} className="svg-icon star"/>
+                        <FontAwesomeIcon icon={faStar} className="svg-icon star" />
                         {serviceRating}
                     </p>
                     <p className='price'>${servicePrice}/hr</p>
                 </div>
                 <div className="booking-wrapper d-flex justify-content-between">
                     <div className='d-flex'>
-                        <FontAwesomeIcon icon={faHeart} className="svg-icon heart"/>
-                        <FontAwesomeIcon icon={faShare} className="svg-icon share"/>
+                        <FontAwesomeIcon icon={faHeart} className="svg-icon heart" />
+                        <FontAwesomeIcon icon={faShare} className="svg-icon share" />
                     </div>
-                    <CardBtn btnTitle="Book Now"/>                    
+                    <CardBtn btnTitle="Book Now" />
                 </div>
             </div>
         </div>
