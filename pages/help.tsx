61c86730c6367430bbe99ead6b9dcfcba00a7ffe
Samikshya Basnet <68534684+SamikshyaBasnet@users.noreply.github.com>
2022-07-13 15:08:08 +0545

    working on help page card component

--- pages/help.tsx
+++ pages/help.tsx
@@ -4,28 +4,29 @@ import Breadcrum from '@components/common/Breadcrum'
 import { Container, Col, Row, Button } from 'react-bootstrap'
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearchengin } from '@fortawesome/free-brands-svg-icons'
 
 import FormButton from '@components/common/FormButton'
 import InputField from '@components/common/InputField'
-
+import CommonCard from '@components/common/CommonCard'
 import { Form, Formik } from 'formik'
 import React from 'react'
 
 import emailValidationSchema from 'utils/formValidation/emailValidation'
+import { faSearch } from '@fortawesome/pro-regular-svg-icons'
+import { helpCardContent } from 'staticData/helpCardContent'
 
 const Help: NextPage = () => {
   return (
     <Layout title="Help & Support | Cipher">
-      <Breadcrum currentPage="Help & Support" />
-      <div className="help-page">
-        <Container fluid="xl">
-          <div className="help-page__top-container">
-            <Row>
+      <section className="help-page-header">
+        <Container>
+          <Breadcrum currentPage="Help & Support" />
+          <div className="help-page-header__top-container">
+            <Row className="d-flex align-items-center">
               <Col md={6}>
                 <figure className="thumbnail-img">
                   <Image
-                    src="/community/earth.png"
+                    src="/help/main-image.svg"
                     layout="fill"
                     objectFit="cover"
                     alt="earth-image"
@@ -34,7 +35,7 @@ const Help: NextPage = () => {
               </Col>
               <Col md={6}>
                 <h1>What help do you need?</h1>
-                <div className="search">
+                <Col sm={6}>
                   <Formik
                     initialValues={{ email: '' }}
                     validationSchema={emailValidationSchema}
@@ -43,40 +44,85 @@ const Help: NextPage = () => {
                     }}
                   >
                     {({ isSubmitting, errors, touched }) => (
-                      <Form>
+                      <Form className="search">
                         <InputField
                           type="email"
                           name="email"
                           error={errors.email}
                           touch={touched.email}
-                          placeHolder="Search Articles"
+                          placeHolder="Enter your email"
                         />
-                        {/* <div className="btn-wrapper">
-                          <button
-                            type="submit"
-                            className="btn"
-                            disabled={isSubmitting}
-                          >
-                            <FontAwesomeIcon
-                              icon={faSearchengin}
-                              className="svg-icon"
-                            />
-                          </button> 
-                        </div>*/}
+
+                        <button
+                          type="submit"
+                          className="btn"
+                          disabled={isSubmitting}
+                        >
+                          <FontAwesomeIcon
+                            icon={faSearch}
+                            className="svg-icon"
+                          />
+                        </button>
                       </Form>
                     )}
                   </Formik>
-                </div>
+                </Col>
               </Col>
             </Row>
           </div>
-          <div className="help-page__browse-container">
+        </Container>
+      </section>
+      <section className="help-page-content">
+        <Container fluid="xl">
+          <div className="help-page-content__browse-container">
             <h1>Browse help categories</h1>
+            <Row>
+              {helpCardContent &&
+                helpCardContent.map((help) => {
+                  return (
+                    <Col
+                      className="help-card"
+                      // sm={6}
+                      md={4}
+                      // lg={4}
+                      key={help.id}
+                    >
+                      <CommonCard
+                        cardImage={help.cardImage}
+                        cardDescription={help.cardDescription}
+                        cardTitle={help.cardTitle}
+                      />
+                    </Col>
+                  )
+                })}
+            </Row>
           </div>
-          <div className="help-page__contact-container"></div>
+          <div className="help-page__contact-container">
+            <Row>
+              <Col md={6}>
+                <div className="card-block">
+                  <figure className="thumbnail-img">
+                    {/* <Image
+                      //src=""
+                      layout="fill"
+                      // height={300}
+                      //objectFit="cover"
+                      alt="referral-card-image"
+                    /> */}
+                  </figure>
+
+                  <div className="card-block__card-content">
+                    <h2 className="card-title"></h2>
+                    <div className="card-description"></div>
+                  </div>
+                </div>
+              </Col>
+              <Col md={6}></Col>
+            </Row>
+          </div>
           <div className="help-page__blog-container"></div>
         </Container>
-      </div>
+      </section>
     </Layout>
   )
 }
---
+++ staticData/helpCardContent.ts
@@ -0,0 +1,58 @@
+export const helpCardContent = [
+    {
+        id:"0",
+        cardTitle:"Find Tasks",
+        cardImage:"/help/browse1.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+    {
+        id:"1",
+        cardTitle:"Payment Issue",
+        cardImage:"/help/browse2.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+    {
+        id:"1",
+        cardTitle:"Payment Issue",
+        cardImage:"/help/browse3.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+    {
+        id:"0",
+        cardTitle:"Find Tasks",
+        cardImage:"/help/browse1.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+    {
+        id:"1",
+        cardTitle:"Payment Issue",
+        cardImage:"/help/browse2.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+    {
+        id:"1",
+        cardTitle:"Payment Issue",
+        cardImage:"/help/browse3.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+    {
+        id:"0",
+        cardTitle:"Find Tasks",
+        cardImage:"/help/browse1.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+    {
+        id:"1",
+        cardTitle:"Payment Issue",
+        cardImage:"/help/browse2.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+    {
+        id:"1",
+        cardTitle:"Payment Issue",
+        cardImage:"/help/browse3.svg",
+        cardDescription: "Easy way to find what you want to do. If a client doesn’t have the time or exact skills to help you with your project, they can refer another freelancer.",
+    },
+
+]
+
