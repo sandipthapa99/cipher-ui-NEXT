cc6ef354c6e708c78fa7e279d7b5f9e76a6cc3d5
Samikshya Basnet <68534684+SamikshyaBasnet@users.noreply.github.com>
2022-07-19 14:36:21 +0545

    profile page top container css finished

---
+++ staticData/merchantProfileCard.ts
@@ -0,0 +1,22 @@
+export const merchantProfileCardInfo = [
+  {
+    id: '0',
+    merchantImage: '/merchant-profile/tree.svg',
+    merchantJob: 'Gadener',
+    merchantOrganization: 'Garden Services',
+    merchantLocation: 'Koteshwor, Kathmandu, Nepal',
+    merchantBio:
+      'A professional gardener with over 10 years of experience. I specialise in Lasagna gardening, Keyhole gardening, mittleider gardening, square foot gardening, and conventional gardening.',
+    merchantRating: 5,
+    merchantPrice: 35,
+    happyClients: 24,
+    successRate: 30,
+    userReviews: 14,
+    taskCompleted: 30,
+    activeFrom: 8,
+    activeTo: 7,
+    memberSince: 'June 2022',
+    merchantActiveStatus: 'true',
+    moreServices: 'House Cleaning, Kitchen Cleaning and Brooming',
+  },
+]
