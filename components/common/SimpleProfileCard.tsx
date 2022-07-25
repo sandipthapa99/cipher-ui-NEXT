efac613b2ed67a229254a8b809ebf0d02b10f133
darshanman <darshanman@cagtu.com>
2022-07-25 12:17:07 +0545

    simple profile card addded

--- components/common/SimpleProfileCard.tsx
+++ components/common/SimpleProfileCard.tsx
@@ -1,25 +1,16 @@
 import Image from "next/image";
 import { ServiceProviderCardProps } from "types/serviceDetail";
-import CardBtn from "./CardBtn";
-import Link from "next/link";
-import { Button } from "react-bootstrap";
 import BookNowButton from "./BookNowButton";
 import { useState } from "react";
-import ModalCard from "./ModalCard";
-import { BookingDetails } from "staticData/bookNowModalCard";
 const SimpleProfileCard = ({
     image,
     name,
     speciality,
-    successRate,
     startingPrice,
-    address,
-    views,
-    happyClients,
 }: ServiceProviderCardProps) => {
     const [showModal, setShowModal] = useState(false);
     return (
-        <div className="card-block d-flex align-items-stretch">
+        <div className="simple-card-block">
             <div className="profile">
                 <figure className="thumbnail-img">
                     <Image
@@ -38,12 +29,6 @@ const SimpleProfileCard = ({
                 <p>Starting price</p>
                 <p className="price">Rs {startingPrice}</p>
             </div>
-            <BookNowButton
-                btnTitle="Book Now"
-                backgroundColor="$primary-color"
-                showModal={true}
-                handleOnClick={() => setShowModal(!showModal)}
-            />
         </div>
     );
 };
