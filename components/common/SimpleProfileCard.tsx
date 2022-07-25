89d142e9f6c44c45a9445c559dd8b177c5250bca
darshanman <darshanman@cagtu.com>
2022-07-25 14:18:15 +0545

    simpleProfileCard completed

--- components/common/SimpleProfileCard.tsx
+++ components/common/SimpleProfileCard.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 import { ServiceProviderCardProps } from "types/serviceDetail";
 import BookNowButton from "./BookNowButton";
 import { useState } from "react";
+import CardBtn from "./CardBtn";
 const SimpleProfileCard = ({
     image,
     name,
     speciality,
     startingPrice,
 }: ServiceProviderCardProps) => {
-    const [showModal, setShowModal] = useState(false);
     return (
-        <div className="simple-card-block">
-            <div className="profile">
+        <div className="simple-card my-5 my-lg-0 ">
+            <div className="d-flex align-items-center simple-card__profile">
                 <figure className="thumbnail-img">
                     <Image
                         src={image}
@@ -25,10 +25,12 @@ const SimpleProfileCard = ({
                     <p className="job">{speciality}</p>
                 </div>
             </div>
-            <div className="starting-price">
-                <p>Starting price</p>
-                <p className="price">Rs {startingPrice}</p>
+            <div className="d-flex justify-content-around align-items-center flex-column flex-sm-row p-4 simple-card__price">
+                <span>Starting price</span>
+                <span className="price">Rs {startingPrice}</span>
             </div>
+                <CardBtn btnTitle={"Apply Now"} backgroundColor={"#38C675"} />
+
         </div>
     );
 };
