d6c252ba99ed9e5e73c050ef761723e3bd98f332
darshanman <darshanman@cagtu.com>
2022-07-26 11:50:33 +0545

    added billingLocation

---
+++ components/Checkout/BillingLocation.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { faLocationDot, faCheck } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AnchorButton from "@components/common/AnchorButton";
+
+const BillingLocation = () => {
+    return (
+        <div className="billing-location">
+            <h2>Billing Location</h2>
+            <div className="d-flex flex-column flex-sm-row justify-content-between align-items-start align-items-sm-center billing-location__location">
+                <span>
+                    <FontAwesomeIcon
+                        icon={faLocationDot}
+                        className="svg-icon-location"
+                    />
+                    Buddhanagar, Kathmandu
+                </span>
+                <AnchorButton className={"px-5"} href={""}>
+                    Change
+                </AnchorButton>
+            </div>
+            <span>
+                <FontAwesomeIcon
+                    icon={faCheck}
+                    className="me-3 svg-icon-check"
+                />
+                Same as Task Location
+            </span>
+        </div>
+    );
+};
+
+export default BillingLocation;
---
+++ pages/task/checkout.tsx
@@ -0,0 +1,34 @@
+import BillingLocation from "@components/Checkout/BillingLocation";
+import Breadcrum from "@components/common/Breadcrum";
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+import SearchHeader from "@components/SearchTask/searchHeader";
+import React, { Fragment } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Checkout = () => {
+    return (
+        <Fragment>
+            <SearchHeader />
+            <Header />
+            <section
+                id="task-checkout-section"
+                className="task-checkout-section"
+            >
+                <Container fluid="xl">
+                    <Breadcrum currentPage={"Checkout"} />
+                    <h1>Checkout</h1>
+                    <Row>
+                        <Col lg={7}>
+                            <BillingLocation />
+                        </Col>
+                        <Col lg={5}>asdsad</Col>
+                    </Row>
+                </Container>
+            </section>
+            <Footer />
+        </Fragment>
+    );
+};
+
+export default Checkout;
