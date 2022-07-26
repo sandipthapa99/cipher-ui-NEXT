d13cae3a72940e7efdda13551b04db587fa42136
darshanman <darshanman@cagtu.com>
2022-07-26 15:47:17 +0545

    anchor button varient added

--- components/common/AnchorButton.tsx
+++ components/common/AnchorButton.tsx
@@ -6,13 +6,19 @@ interface AnchorButtonProps {
     children: ReactNode;
     className: string;
     href: string;
+    varient: "secondary" | "";
 }
 
-const AnchorButton = ({ children, href, className }: AnchorButtonProps) => {
+const AnchorButton = ({
+    children,
+    href,
+    className,
+    varient,
+}: AnchorButtonProps) => {
     return (
         <>
             <Link href={href}>
-                <a className={`btn anchor-button ${className}`}>
+                <a className={`btn anchor-button ${className} ${varient}`}>
                     <span>{children}</span>
                 </a>
             </Link>
