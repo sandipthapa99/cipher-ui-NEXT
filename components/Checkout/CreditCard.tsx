f58559ca961175551a2969fa68f1b67a93a142b6
darshanman <darshanman@cagtu.com>
2022-07-27 16:49:34 +0545

    changes on credit component

--- components/Checkout/CreditCard.tsx
+++ components/Checkout/CreditCard.tsx
@@ -6,7 +6,7 @@ const CreditCard = ({cardDetail}: {cardDetail: CreditCardContent[0]}) => {
   return (
     <div className='credit-card-wrapper mt-3 mt-md-0 me-5'>
         <h3>{cardDetail.number}</h3>
-        <div className='d-flex justify-content-between credit-card-detail'>        
+        <div className='d-flex flex-column-reverse flex-sm-row justify-content-between credit-card-detail'>        
             <div className='d-flex flex-column'>
                 <span>{cardDetail.name}</span>
                 <span>{cardDetail.expDate}</span>
