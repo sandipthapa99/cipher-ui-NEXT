31c6c54cf84da01dce19f73edbd93ed68dccfad7
darshanman <darshanman@cagtu.com>
2022-07-28 09:17:19 +0545

    paymet types defined

--- components/Checkout/PaymentMethod.tsx
+++ components/Checkout/PaymentMethod.tsx
@@ -1,92 +1,123 @@
-import Link from 'next/link'
-import React, { useContext } from 'react'
-import CreditCard from './CreditCard'
-import {creditCardContent, CreditCardContent} from 'staticData/creditCardContent'
-import { Accordion, AccordionContext, Card, useAccordionButton } from 'react-bootstrap'
-import {faCircleDot} from '@fortawesome/pro-regular-svg-icons'
-import {faCircleDot as circleDot} from '@fortawesome/pro-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from "next/link";
+import React, { ReactNode, useContext } from "react";
+import CreditCard from "./CreditCard";
+import {
+    creditCardContent,
+    CreditCardContent,
+} from "staticData/creditCardContent";
+import {
+    Accordion,
+    AccordionContext,
+    Card,
+    useAccordionButton,
+} from "react-bootstrap";
+import { faCircleDot } from "@fortawesome/pro-regular-svg-icons";
+import { faCircleDot as circleDot } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function ContextAwareToggle({ children, eventKey, callback }) {
-  const { activeEventKey } = useContext(AccordionContext);
+interface PaymentAccordion {
+    children: ReactNode;
+    eventKey: string;
+    callback: any;
+}
 
-  const decoratedOnClick = useAccordionButton(
+function ContextAwareToggle({
+    children,
     eventKey,
-    () => callback && callback(eventKey),
-  );
+    callback,
+}: PaymentAccordion) {
+    const { activeEventKey } = useContext(AccordionContext);
 
-  const isCurrentEventKey = activeEventKey === eventKey;
+    const decoratedOnClick = useAccordionButton(
+        eventKey,
+        () => callback && callback(eventKey)
+    );
 
-  return (
-    <button
-      type="button"
-      onClick={decoratedOnClick}
-    >
-      {isCurrentEventKey?<FontAwesomeIcon icon={circleDot} className='svg-icon-boolen-circle me-3'/>:<FontAwesomeIcon icon={faCircleDot} className='svg-icon-boolen-circle me-3'/>}
-      {children}
-    </button>
-  );
+    const isCurrentEventKey = activeEventKey === eventKey;
+
+    return (
+        <button type="button" onClick={decoratedOnClick}>
+            {isCurrentEventKey ? (
+                <FontAwesomeIcon
+                    icon={circleDot}
+                    className="svg-icon-boolen-circle me-3"
+                />
+            ) : (
+                <FontAwesomeIcon
+                    icon={faCircleDot}
+                    className="svg-icon-boolen-circle me-3"
+                />
+            )}
+            {children}
+        </button>
+    );
 }
 
 const PaymentMethod = () => {
-  return (
-    <div className='payment-method mt-5'>
-      <h2>
-        Payment Methods
-      </h2>
-        <div className='payment-method-wraper__cards'>
-          <Accordion defaultActiveKey="0">
-            <Card>
-              <Card.Header className='d-flex flex-column-reverse align-items-center flex-md-row justify-content-between'>
-                <ContextAwareToggle eventKey="0" callback={''}>                
-                    <span className='my-3 my-md-0'>Debit/Credit Card (2)</span>
-                </ContextAwareToggle>
-                <Link href={'/'}>
-                    <a>
-                      + Add New 
-                    </a>
-                </Link>
-              </Card.Header>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>
-
-                  <div className='d-flex flex-column flex-md-row'>
-                  {creditCardContent &&
-                      creditCardContent.map((card, key) => (
-                        <CreditCard key={key} cardDetail={card}/>
-                      ))}
-                </div> 
-              </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <ContextAwareToggle eventKey="1" callback={''}> Digital Wallet (3)</ContextAwareToggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>Hello! I'm another body</Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <ContextAwareToggle eventKey="2" callback={''}>Linked Bank (2)</ContextAwareToggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="2">
-                <Card.Body>Hello! I'm another body</Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <ContextAwareToggle eventKey="3" callback={''}>Other Methods (3)</ContextAwareToggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="3">
-                <Card.Body>Hello! I'm another body</Card.Body>
-            </Accordion.Collapse>
-            </Card>
-        </Accordion>   
+    return (
+        <div className="payment-method mt-5">
+            <h2>Payment Methods</h2>
+            <div className="payment-method-wraper__cards">
+                <Accordion defaultActiveKey="0">
+                    <Card>
+                        <Card.Header className="d-flex flex-column-reverse align-items-center flex-md-row justify-content-between">
+                            <ContextAwareToggle eventKey="0" callback={""}>
+                                <span className="my-3 my-md-0">
+                                    Debit/Credit Card (2)
+                                </span>
+                            </ContextAwareToggle>
+                            <Link href={"/"}>
+                                <a>+ Add New</a>
+                            </Link>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <div className="d-flex flex-column flex-md-row">
+                                    {creditCardContent &&
+                                        creditCardContent.map((card, key) => (
+                                            <CreditCard
+                                                key={key}
+                                                cardDetail={card}
+                                            />
+                                        ))}
+                                </div>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            <ContextAwareToggle eventKey="1" callback={""}>
+                                Digital Wallet (3)
+                            </ContextAwareToggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            <ContextAwareToggle eventKey="2" callback={""}>
+                                Linked Bank (2)
+                            </ContextAwareToggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="2">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            <ContextAwareToggle eventKey="3" callback={""}>
+                                Other Methods (3)
+                            </ContextAwareToggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="3">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+            </div>
         </div>
-      </div>
-  )
-}
+    );
+};
 
-export default PaymentMethod
+export default PaymentMethod;
