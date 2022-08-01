778a758c4e3b0e323731f9ff7c9f526806e21ae9
Samikshya <samikshyabasnet@cagtu.com>
2022-08-01 10:33:07 +0545

    gradient layout css refractored

--- components/GradientBanner.tsx
+++ components/GradientBanner.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Col, Row } from "react-bootstrap";
 
 import CardBtn from "./common/CardBtn";
 
@@ -9,30 +10,29 @@ interface GradientBannerProps {
 }
 const GradientBanner = ({ title, subTitle, image }: GradientBannerProps) => {
     return (
-        <>
-            <section className="explore-services__top-container">
-                <div className="gradient"></div>
-                <figure className="thumbnail-img">
-                    <Image
-                        src={image}
-                        layout="fill"
-                        objectFit="cover"
-                        alt="oppurtunities-page-main-image"
-                    />
-                </figure>
-                <div className="overlay pb-3">
-                    <h1>{title}</h1>
-                    <div className="bottom-content">
-                        <p>{subTitle}</p>
-                    </div>
-                    <CardBtn
-                        btnTitle="Join Us"
-                        backgroundColor="#FFF"
-                        color="#000"
-                    />
+        <div className="gradient-layout">
+            <div className="gradient"></div>
+            <figure className="thumbnail-img">
+                <Image
+                    src={image}
+                    layout="fill"
+                    objectFit="cover"
+                    alt="oppurtunities-page-main-image"
+                />
+            </figure>
+
+            <div className="overlay pb-3">
+                <h1>{title}</h1>
+                <div className="bottom-content">
+                    <p>{subTitle}</p>
                 </div>
-            </section>
-        </>
+                <CardBtn
+                    btnTitle="Join Us"
+                    backgroundColor="#FFF"
+                    color="#000"
+                />
+            </div>
+        </div>
     );
 };
 export default GradientBanner;
