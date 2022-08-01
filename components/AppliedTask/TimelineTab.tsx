24656041d796a61cac8143d159832d6aa2a2dddb
Subash Poudel <subashpoudel@cagtu.com>
2022-08-01 17:49:22 +0545

    pass functions through props to each timeline from timeline tab

--- components/AppliedTask/TimelineTab.tsx
+++ components/AppliedTask/TimelineTab.tsx
@@ -4,12 +4,25 @@ import { EachTimeline } from "./EachTimeline";
 
 export const TimelineTab = () => {
     const [activeId, setActiveId] = useState<number>(1);
+    const [isGivingRevision, setIsGivingRevision] = useState(false);
 
-      
+    const handleRevisionOpen = () => {
+        setIsGivingRevision(true);
+    };
+
     return (
         <div className="timeline-tab-component">
             {TaskDetailTimelineData.map((item) => (
-                <EachTimeline {...item} key={item?.id} buttonName="Request for revision" activeId = {activeId} setActiveId={setActiveId} />
+                <EachTimeline
+                    {...item}
+                    key={item?.id}
+                    buttonName="Request for revision"
+                    activeId={activeId}
+                    setActiveId={setActiveId}
+                    isGivingRevision={isGivingRevision}
+                    handleRevisionOpen={handleRevisionOpen}
+                    setIsGivingRevision={setIsGivingRevision}
+                />
             ))}
         </div>
     );
