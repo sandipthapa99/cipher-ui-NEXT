e19a37e888481c77c526c06c42155b2a11470be7
Subash Poudel <subashpoudel@cagtu.com>
2022-08-01 14:46:08 +0545

    report review form component

---
+++ components/AppliedTask/ReportRevisionForm.tsx
@@ -0,0 +1,109 @@
+import BigButton from "@components/common/Button";
+import FormButton from "@components/common/FormButton";
+import InputField from "@components/common/InputField";
+import SelectInputField from "@components/common/SelectInputField";
+import { Form, Formik } from "formik";
+import Image from "next/image";
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { SelectOptionProps } from "types/selectInputField";
+
+interface ReportRevisionFormProps {
+    show: boolean;
+    handleClose: () => void;
+}
+
+export const ReportRevisionForm = ({
+    show,
+    handleClose,
+}: ReportRevisionFormProps) => {
+    const initialValues = {
+        report_type: "",
+        revision_reason: "",
+    };
+
+    const options: SelectOptionProps[] = [
+        {
+            id: 1,
+            value: "Request for Refund",
+            label: "Request for Refund",
+        },
+
+        {
+            id: 2,
+            value: "No request refund",
+            label: "No request refund",
+        },
+    ];
+
+    return (
+        <div className="collaboration-request-form">
+            <Modal
+                show={show}
+                onHide={handleClose}
+                className="collaboration-request-modal"
+            >
+                <Modal.Header closeButton></Modal.Header>
+                <Modal.Body>
+                    <div className="revision-title-section">
+                        <h3>Report</h3>
+
+                        <p>Send report for this Task</p>
+
+                        <figure className="revision_report_image">
+                            <Image
+                                src={"/heroImages/revision_report_image.png"}
+                                alt="revision_report_image"
+                                height={150}
+                                width={150}
+                            />
+                        </figure>
+                    </div>
+
+                    <div className="form-section">
+                        <Formik
+                            initialValues={initialValues}
+                            onSubmit={async (values) => {
+                                console.log(values);
+                            }}
+                        >
+                            {({ setFieldValue, errors }) => {
+                                return (
+                                    <Form>
+                                        <SelectInputField
+                                            name="report_type"
+                                            labelName="Select report type"
+                                            options={options}
+                                            placeHolder="Request for Refund"
+                                        />
+
+                                        <InputField
+                                            name="reason"
+                                            labelName="Reason"
+                                            placeHolder="Write your reason here"
+                                            as="textarea"
+                                        />
+
+                                        <div className="d-flex justify-content-center button-section">
+                                            <span className="cancel-button">
+                                                <BigButton
+                                                    btnTitle={"Cancel"}
+                                                    backgroundColor={"white"}
+                                                    handleClick={handleClose}
+                                                />
+                                            </span>
+                                            <FormButton
+                                                handleClick={handleClose}
+                                                name={"Request"}
+                                            />
+                                        </div>
+                                    </Form>
+                                );
+                            }}
+                        </Formik>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
+};
