a6c17eac5571c05df1b199fe956793436cc8a734
Subash Poudel <subashpoudel@cagtu.com>
2022-08-03 15:03:43 +0545

    new ApplyPostComponent created

---
+++ components/common/ApplyPostComponent.tsx
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderOpen } from "@fortawesome/pro-regular-svg-icons";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import PostModal from "@components/PostTask/PostModal";
+
+export const ApplyPostComponent = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShow = () => setShowModal(true);
+    const handleClose = () => setShowModal(false);
+
+    return (
+        <div className="apply-post">
+            <div className="folder">
+                <FontAwesomeIcon icon={faFolderOpen} className="folder-icon" />
+            </div>
+            <div className="text-post">
+                <p className="head">No active Task posts</p>
+                <p className="para">
+                    Post a task to the marketplace and let merchant come to you.
+                </p>
+            </div>
+            <div className="btn-cont">
+                <Button
+                    variant="light"
+                    className="post-btn"
+                    onClick={handleShow}
+                >
+                    Post a Task
+                </Button>
+            </div>
+            <Modal
+                show={showModal}
+                onHide={handleClose}
+                backdrop="static"
+                className="post-modal"
+            >
+                <Modal.Header className="mt-4" closeButton></Modal.Header>
+                <Modal.Body>
+                    <PostModal onSubmit={handleClose} />
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
+};
