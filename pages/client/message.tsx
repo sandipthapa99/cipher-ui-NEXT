99e1fbdf3e7b4d008af20d50ba3e4fe10bbb29c7
Sachin Aryal <sachinaryal@cagtu.com>
2022-08-04 10:09:46 +0545

    Added client message page

---
+++ pages/client/message.tsx
@@ -0,0 +1,10 @@
+import { Container } from "react-bootstrap";
+
+export const ClientMessagePage = () => {
+    return (
+        <Container>
+            <h4 className="title">Messages</h4>
+        </Container>
+    );
+};
+export default ClientMessagePage;
