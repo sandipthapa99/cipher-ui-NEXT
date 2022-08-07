df5317dbc5620275b4d7087b0463e6453c2722af
darshanman <darshanman@cagtu.com>
2022-08-07 12:13:37 +0545

    feature/career careerCard added

---
+++ staticData/careerCardValues.ts
@@ -0,0 +1,36 @@
+export const careerCardValues = [
+    {
+        id: "0",
+        Title: "Guidelines",
+    },
+    {
+        id: "1",
+        Title: "Sr. Front End Developer",
+    },
+    {
+        id: "2",
+        Title: "Guidelines",
+    },
+    {
+        id: "3",
+        Title: "Sr. Front End Developer",
+    },
+    {
+        id: "4",
+        Title: "Guidelines",
+    },
+    {
+        id: "5",
+        Title: "Sr. Front End Developer",
+    },
+    {
+        id: "6",
+        Title: "Sr. Front End Developer",
+    },
+    {
+        id: "7",
+        Title: "Sr. Front End Developer",
+    },
+];
+
+export type CareerCardProps = typeof careerCardValues[0];
