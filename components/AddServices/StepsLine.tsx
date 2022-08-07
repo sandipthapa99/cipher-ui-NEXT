55cb28b6339a6dde3ae407f4c956a93385509cc2
Subash Poudel <subashpoudel@cagtu.com>
2022-08-07 11:51:09 +0545

    making steplines responsive

--- components/AddServices/StepsLine.tsx
+++ components/AddServices/StepsLine.tsx
@@ -42,69 +42,83 @@ export const StepsLine = ({ step }: { step: number }) => {
         <section id="steps-line-add-service" className="steps-line-add-service">
             <Container>
                 <div className="d-flex align-items-center mt-5 steps-line">
-                    <span
-                        className={oneActive ? "step-one__active" : "step-one"}
-                    >
-                        1
-                    </span>
-                    <span
-                        className={
-                            oneActive
-                                ? "step-name-one__active"
-                                : "step-name-one"
-                        }
-                    >
-                        Details
-                    </span>
+                    <div className="d-flex one">
+                        <span
+                            className={
+                                oneActive ? "step-one__active" : "step-one"
+                            }
+                        >
+                            1
+                        </span>
+                        <span
+                            className={
+                                oneActive
+                                    ? "step-name-one__active"
+                                    : "step-name-one"
+                            }
+                        >
+                            Details
+                        </span>
+                    </div>
                     <FontAwesomeIcon icon={faAngleRight} />
-                    <span
-                        className={twoActive ? "step-two__active" : "step-two"}
-                    >
-                        2
-                    </span>
-                    <span
-                        className={
-                            twoActive
-                                ? "step-name-two__active"
-                                : "step-name-two"
-                        }
-                    >
-                        Pricing
-                    </span>
+                    <div className="d-flex two">
+                        <span
+                            className={
+                                twoActive ? "step-two__active" : "step-two"
+                            }
+                        >
+                            2
+                        </span>
+                        <span
+                            className={
+                                twoActive
+                                    ? "step-name-two__active"
+                                    : "step-name-two"
+                            }
+                        >
+                            Pricing
+                        </span>
+                    </div>
                     <FontAwesomeIcon icon={faAngleRight} />
-                    <span
-                        className={
-                            threeActive ? "step-three__active" : "step-three"
-                        }
-                    >
-                        3
-                    </span>
-                    <span
-                        className={
-                            threeActive
-                                ? "step-name-three__active"
-                                : "step-name-three"
-                        }
-                    >
-                        Gallery
-                    </span>
+                    <div className="d-flex three">
+                        <span
+                            className={
+                                threeActive
+                                    ? "step-three__active"
+                                    : "step-three"
+                            }
+                        >
+                            3
+                        </span>
+                        <span
+                            className={
+                                threeActive
+                                    ? "step-name-three__active"
+                                    : "step-name-three"
+                            }
+                        >
+                            Gallery
+                        </span>
+                    </div>
                     <FontAwesomeIcon icon={faAngleRight} />
-                    <span
-                        className={
-                            fourActive ? "step-four__active" : "step-four"
-                        }
-                    >
-                        4
-                    </span>
-                    <span
-                        className={
-                            fourActive
-                                ? "step-name-four__active"
-                                : "step-name-four"
-                        }
-                    >
-                        Publish
-                    </span>
+                    <div className="d-flex four">
+                        <span
+                            className={
+                                fourActive ? "step-four__active" : "step-four"
+                            }
+                        >
+                            4
+                        </span>
+                        <span
+                            className={
+                                fourActive
+                                    ? "step-name-four__active"
+                                    : "step-name-four"
+                            }
+                        >
+                            Publish
+                        </span>
+                    </div>
                 </div>
             </Container>
         </section>
