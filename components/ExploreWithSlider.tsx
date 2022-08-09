2026fe1322c299738c0a7b9b171e3b3ca5ad672f
Subash Poudel <subashpoudel@cagtu.com>
2022-08-09 12:53:22 +0545

    win new clients slider component

---
+++ components/ExploreWithSlider.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import { WinNewClientData } from "staticData/growBusiness";
+import { WinClientCard } from "./common/WinClientCard";
+
+export const ExploreWithSlider = () => {
+    return (
+        <div className="explore-with-slider">
+            <Carousel>
+                {WinNewClientData &&
+                    WinNewClientData.map((item) => (
+                        <Carousel.Item>
+                            <WinClientCard
+                                image={item.image}
+                                title={item.title}
+                                description={item.description}
+                                buttonText={item.buttonText}
+                            />
+                        </Carousel.Item>
+                    ))}
+            </Carousel>
+        </div>
+    );
+};
