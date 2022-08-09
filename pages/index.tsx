53c554bd4afab2238d07adb7a804b0c302a3cf10
darshanman <darshanman@cagtu.com>
2022-08-09 17:32:27 +0545

    bugfix/marguee added

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 import { blogCardContent } from "staticData/community";
 import { findHire } from "staticData/findHire";
 import { merchants } from "staticData/merchants";
@@ -161,15 +162,15 @@ const Home: NextPage = () => {
                 id="cagtu-cipher-buzz-section"
                 className="cagtu-cipher-buzz-section"
             >
-                <Container fluid="xl" className="px-5">
-                    <div className="d-flex justify-content-around list-bar">
+                <Container fluid="xl">
+                    <Marquee gradient={false} className="marquee">
                         <li className="light">Cagtu</li>
                         <li className="strong">Cipher</li>
                         <li className="light">Code Sharav</li>
                         <li className="strong">Buzz</li>
                         <li className="light">Cipher</li>
                         <li className="strong">Code Sharav</li>
-                    </div>
+                    </Marquee>
                 </Container>
             </section>
 
