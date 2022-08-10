bb7fc1746708c3f3f0fa5ec337acf63ce9078188
darshanman <darshanman@cagtu.com>
2022-08-10 10:12:51 +0545

    bugfix/breadcrumb dynamic breadcrumb added

---
+++ components/common/BreadCrumb.tsx
@@ -0,0 +1,36 @@
+import { faChevronRight } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import { Breadcrumb, Container } from "react-bootstrap";
+import type { breadCrumbProps } from "types/breadCrumbProps";
+
+export const BreadCrumb = ({ currentPage }: breadCrumbProps) => {
+    const router = useRouter();
+    const routes = router.asPath.split("/").filter((route) => route);
+
+    const breadCrumbRoutes = routes.slice(0, routes.length - 1);
+    return (
+        <section id="breadcrumb-section" className="breadcrumb-section">
+            <Container fluid="xl" className="px-5">
+                <Breadcrumb>
+                    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                    <FontAwesomeIcon icon={faChevronRight} />
+                    {breadCrumbRoutes.map((route, key) => (
+                        <span key={key}>
+                            <Link href={`/${route.toLowerCase()}`}>
+                                <a>{route}</a>
+                            </Link>
+                            <FontAwesomeIcon
+                                icon={faChevronRight}
+                                className="ps-3"
+                            />
+                        </span>
+                    ))}
+                    <Breadcrumb.Item active>{currentPage}</Breadcrumb.Item>
+                </Breadcrumb>
+            </Container>
+        </section>
+    );
+};
