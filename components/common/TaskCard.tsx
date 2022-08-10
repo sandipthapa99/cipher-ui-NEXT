966cd7f1a289078c4e93e73efd415a2f5a82fd1b
Subash Poudel <subashpoudel@cagtu.com>
2022-08-10 15:11:55 +0545

    add form on apply button homepage

--- components/common/TaskCard.tsx
+++ components/common/TaskCard.tsx
@@ -1,3 +1,4 @@
+import AppliedForm from "@components/AppliedTask/AppliedForm";
 import {
     faCalendar,
     faClockEight,
@@ -6,9 +7,12 @@ import {
     faUserGroup,
 } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { BookingDetails } from "staticData/bookNowModalCard";
 import type { TaskCardProps } from "types/taskCard";
 
 import CardBtn from "./CardBtn";
+import ShareIcon from "./ShareIcon";
 // css for this file is done in _gettingStartedTask.scss page
 const TaskCard = ({
     title,
@@ -20,6 +24,7 @@ const TaskCard = ({
     isCompleted,
     isRunning,
 }: TaskCardProps) => {
+    const [showModal, setShowModal] = useState(false);
     return (
         <div className="task-card-block">
             <div className="task-card-block__header d-flex flex-column flex-sm-row justify-content-between">
@@ -55,7 +60,7 @@ const TaskCard = ({
             <div className="task-card-block__footer d-flex flex-column flex-sm-row justify-content-between">
                 <div className="left d-flex align-items-center">
                     <p className="share d-flex align-items-center">
-                        <FontAwesomeIcon icon={faShare} className="svg-icon" />
+                        <ShareIcon />
                         Share
                     </p>
                     <p className="applicants  d-flex align-items-center">
@@ -75,10 +80,27 @@ const TaskCard = ({
                             }
                         />
                     ) : (
-                        <CardBtn btnTitle={"Apply"} backgroundColor="#38C675" />
+                        <CardBtn
+                            btnTitle={"Apply"}
+                            backgroundColor="#38C675"
+                            handleClick={() => setShowModal(true)}
+                        />
                     )}
                 </div>
             </div>
+
+            {BookingDetails &&
+                BookingDetails.map((detail) => (
+                    <AppliedForm
+                        key={detail.id}
+                        title={detail.title}
+                        price={detail.price}
+                        image={detail.image}
+                        description={detail.description}
+                        show={showModal}
+                        handleClose={() => setShowModal(false)}
+                    />
+                ))}
         </div>
     );
 };
