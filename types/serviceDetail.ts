875cc7b438541483115eeaf2925e11a3429d8468
Subash Poudel <subashpoudel@cagtu.com>
2022-08-11 15:08:53 +0545

    types updated in service details and task card props

--- types/serviceDetail.ts
+++ types/serviceDetail.ts
@@ -1,13 +1,15 @@
 export interface ServiceProviderCardProps {
-    name: string;
-    speciality: string;
-    address: string;
-    views: number;
-    happyClients: number;
-    successRate: number;
-    startingPrice: number | string;
+    name?: string;
+    speciality?: string;
+    address?: string;
+    views?: number;
+    happyClients?: number;
+    successRate?: number;
+    startingPrice?: number | string;
+    endPrice?: number | string;
     image: string;
     isApplied?: boolean;
     isPermission?: boolean;
     isAddServiceForm?: boolean;
+    currency?:string;
 }
