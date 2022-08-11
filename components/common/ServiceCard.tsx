b837f8919205d556d55bfd57f6ded11fb28bcda5
darshanman <darshanman@cagtu.com>
2022-08-11 11:11:28 +0545

    feature/career career-detail listing compeleted

--- components/common/ServiceCard.tsx
+++ components/common/ServiceCard.tsx
@@ -70,7 +70,6 @@ ServiceCardProps) => {
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="d-flex align-items-center justify-content-around justify-content-md-between mb-3 mb-sm-0">
                         <SaveIcon />
-
                         <ShareIcon />
                     </div>
                     <CardBtn btnTitle="Book Now" backgroundColor="#211D4F" />
--- pages/career/[slug].tsx
+++ pages/career/[slug].tsx
@@ -3,34 +3,39 @@ import LeaveYourCV from "@components/Career/LeaveYourCV";
 import AnchorButton from "@components/common/AnchorButton";
 import { BreadCrumb } from "@components/common/BreadCrumb";
 import Layout from "@components/Layout";
+import type { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
-import { useRouter } from "next/router";
 import React from "react";
 import { Container } from "react-bootstrap";
-import { careerCardValues } from "staticData/careerCardValues";
+import type {
+    CareerDetailsData,
+    CareerValueProps,
+} from "types/careerValuesProps";
+import { axiosClient } from "utils/axiosClient";
 
-const CareerDeatils = () => {
-    const router = useRouter();
-    const { id } = router.query;
-    const careerCard = careerCardValues.find((item) => item.id === id);
+const CareerDeatils = ({
+    career,
+}: {
+    career: CareerValueProps["result"][0];
+}) => {
     return (
         <Layout title="Cipher | Careers">
             <section id="careers-details-section" className="careers-section">
-                <BreadCrumb currentPage={careerCard?.Title} />
+                <BreadCrumb currentPage={career?.title} />
                 <Container fluid="xl">
                     <div className="careers-detail bg-white">
-                        <h1>{careerCard?.Title}</h1>
+                        <h1>{career?.title}</h1>
                         <p>
                             <span>Location:</span>
-                            {careerCard?.data.location}
+                            {career?.location}
                         </p>
                         <p>
                             <span>Work Type:</span>
-                            {careerCard?.data.workType}
+                            {career?.job_type}
                         </p>
                         <h2>What’s the job?</h2>
                         <div className="d-flex justify-content-between align-items-center careers-detail__job">
-                            <span>{careerCard?.data.jobDescription}</span>
+                            <span>{career?.description}</span>
                             <figure>
                                 <Image
                                     src={"/joinTeam.png"}
@@ -40,17 +45,9 @@ const CareerDeatils = () => {
                             </figure>
                         </div>
                         <h2>What am I going to do?</h2>
-                        <ul>
-                            {careerCard?.data.work.map((item, key) => (
-                                <li key={key}>{item}</li>
-                            ))}
-                        </ul>
+                        <ul>{career?.title}</ul>
                         <h2>What are the qualifications?</h2>
-                        <ul>
-                            {careerCard?.data.qualification.map((item, key) => (
-                                <li key={key}>{item}</li>
-                            ))}
-                        </ul>
+                        <ul>{career?.title}</ul>
                         <AnchorButton
                             className={"big-btn"}
                             href={"/career/apply"}
@@ -68,3 +65,46 @@ const CareerDeatils = () => {
 };
 
 export default CareerDeatils;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    try {
+        const { data: careerData } = await axiosClient.get(
+            "/career/vacancy/list/"
+        );
+        const paths = careerData?.result?.map(
+            ({ slug }: CareerValueProps["result"][0]) => ({
+                params: { slug: slug },
+            })
+        );
+        return { paths, fallback: true };
+    } catch (error: any) {
+        error.response.data;
+        return {
+            paths: [],
+            fallback: true,
+        };
+    }
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    try {
+        const { data } = await axiosClient.get<CareerDetailsData>(
+            `/career/vacancy/detail/${params?.slug}`
+        );
+
+        return {
+            props: {
+                career: data.data,
+            },
+            revalidate: 10,
+        };
+    } catch (error: any) {
+        error.response.data;
+        return {
+            props: {
+                career: {},
+            },
+            revalidate: 10,
+        };
+    }
+};
