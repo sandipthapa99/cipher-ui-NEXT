c8134241338c69ade1a85b32b275a199eb6d515e
Subash Poudel <subashpoudel@cagtu.com>
2022-08-14 16:37:26 +0545

    useSupport hook for post support tickets

---
+++ hooks/support/useSupport.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { SupportValuesProps } from "types/contact";
+import { axiosClient } from "utils/axiosClient";
+
+export const useSupport = () => {
+    return useMutation<void, Error, SupportValuesProps>(async (supportDetails) => {
+        try {
+            const { data } = await axiosClient.post("/support/support-ticket/", supportDetails);
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                throw new Error(error?.response?.data?.message);
+            }
+            throw new Error("Something went wrong");
+        }
+    });
+};
