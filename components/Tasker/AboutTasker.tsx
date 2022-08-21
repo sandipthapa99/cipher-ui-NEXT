8c86290965075d40eb0bc58340dcd9c4dfde48ee
Subash Poudel <subashpoudel@cagtu.com>
2022-08-21 11:55:31 +0545

    About tab section UI development in tasker page

---
+++ components/Tasker/AboutTasker.tsx
@@ -0,0 +1,297 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Col, Row } from "react-bootstrap";
+import { UserProfileProps } from "types/userProfileProps";
+import { useGetProfile } from "hooks/profile/useGetProfile";
+import { useData } from "hooks/use-data";
+import { format } from "date-fns";
+
+export const AboutTasker = () => {
+    //user profile certification data
+    const { data: certificationData } = useData<
+        UserProfileProps["certificationData"]
+    >(["tasker-certification"], "/tasker/certification/");
+
+    //user profile education data
+    const { data: educationData } = useData<UserProfileProps["educationData"]>(
+        ["tasker-education"],
+        "/tasker/education/"
+    );
+
+    //user profile experience data
+    const { data: experienceData } = useData<
+        UserProfileProps["experienceData"]
+    >(["tasker-experience"], "/tasker/experience/");
+
+    //user profile experience data
+    const { data: portfolioData } = useData<UserProfileProps["portfolioData"]>(
+        ["tasker-portfolio"],
+        "/tasker/portfolio/"
+    );
+
+    const { data: profileDetails } = useGetProfile();
+
+    console.log("profile=", profileDetails);
+    const userSkills = profileDetails ? JSON.parse(profileDetails?.skill) : [];
+    console.log("user profile skills=", userSkills);
+
+    return (
+        <>
+            <div className="about-profile">
+                <div className="type portfolio">
+                    <div className="title-wrapper d-flex justify-content-between">
+                        {/* <h2 className="heading-title">Community activity</h2> */}
+                        <h1>My Portfolio</h1>
+                    </div>
+
+                    <div className="content">
+                        {portfolioData
+                            ? portfolioData?.data?.result?.map((info: any) => (
+                                  <div className="image" key={info?.id}>
+                                      <Row>
+                                          <Col md={6}>
+                                              <Link href={`${info?.image}`}>
+                                                  <a target="_blank">
+                                                      {info?.image ? (
+                                                          <figure className="thumbnail-img">
+                                                              <Image
+                                                                  src={`${info?.image}`}
+                                                                  layout="fill"
+                                                                  objectFit="cover"
+                                                                  alt="portfolio-image"
+                                                              />
+                                                          </figure>
+                                                      ) : (
+                                                          ""
+                                                      )}
+                                                  </a>
+                                              </Link>
+                                          </Col>
+                                          <Col md={6}>
+                                              <Link href={`${info?.file}`}>
+                                                  <a target="_blank">
+                                                      {info?.file ? (
+                                                          <figure className="thumbnail-img">
+                                                              <Image
+                                                                  src="/userprofile/documents/pdf.svg"
+                                                                  layout="fill"
+                                                                  objectFit="cover"
+                                                                  alt="portfolio-file"
+                                                              />
+                                                          </figure>
+                                                      ) : (
+                                                          ""
+                                                      )}
+                                                  </a>
+                                              </Link>
+                                          </Col>
+                                      </Row>
+
+                                      <p className="text-center">
+                                          {info.title}
+                                      </p>
+                                  </div>
+                              ))
+                            : "Looks like you have no Portfolio data"}
+                    </div>
+                </div>
+                <div className="type experience">
+                    <div className="title-wrapper d-flex justify-content-between">
+                        {/* <h2 className="heading-title">Community activity</h2> */}
+                        <h1>Experience</h1>
+                    </div>
+
+                    <Row>
+                        <Col md={9}>
+                            <div className="content">
+                                {experienceData
+                                    ? experienceData?.data?.result?.map(
+                                          (value, key) => (
+                                              <div
+                                                  className="experience__type"
+                                                  key={key}
+                                              >
+                                                  <div className="name d-flex">
+                                                      <h3>{value?.title}</h3>
+                                                  </div>
+                                                  <div className="company d-flex">
+                                                      <p className="name">
+                                                          {value?.company_name}
+                                                          &nbsp;. &nbsp;
+                                                          {
+                                                              value?.employment_type
+                                                          }
+                                                      </p>
+                                                  </div>
+                                                  <p className="description">
+                                                      {value?.description}
+                                                  </p>
+                                                  <p className="date">
+                                                      {format(
+                                                          new Date(
+                                                              value?.start_date
+                                                          ),
+                                                          "MMMM yyyy"
+                                                      )}
+                                                      {`${
+                                                          value?.end_date
+                                                              ? `-`
+                                                              : "- Present"
+                                                      }`}
+                                                      {value?.end_date &&
+                                                          format(
+                                                              new Date(
+                                                                  value.end_date
+                                                              ),
+                                                              "MMMM yyyy"
+                                                          )}
+                                                  </p>
+                                                  <p className="address">
+                                                      {value.location}
+                                                  </p>
+                                              </div>
+                                          )
+                                      )
+                                    : "Looks like you have no Experience Data"}
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className="type skills">
+                    <div className="title-wrapper d-flex justify-content-between">
+                        {/* <h2 className="heading-title">Community activity</h2> */}
+                        <h1>Skills</h1>
+                    </div>
+
+                    <Row>
+                        <Col md={9}>
+                            <div className="content">
+                                {userSkills.map((info: any, i: any) => (
+                                    <div className="skills__type" key={i}>
+                                        {info}
+                                    </div>
+                                ))}
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className="type education">
+                    <div className="title-wrapper d-flex justify-content-between">
+                        {/* <h2 className="heading-title">Community activity</h2> */}
+                        <h1>Education</h1>
+                    </div>
+                    <Row>
+                        <Col md={9}>
+                            <div className="content">
+                                {educationData
+                                    ? educationData?.data.result.map(
+                                          (value: any, key: number) => (
+                                              <div
+                                                  className="education__type"
+                                                  key={key}
+                                              >
+                                                  <div className="name d-flex">
+                                                      <h3 className="institution">
+                                                          {value.school}
+                                                      </h3>
+                                                  </div>
+                                                  <h3 className="program">
+                                                      {value.degree}
+                                                  </h3>
+
+                                                  <p className="date">
+                                                      {format(
+                                                          new Date(
+                                                              value.start_date
+                                                          ),
+                                                          "MMMM yyyy"
+                                                      )}
+                                                      -
+                                                      {format(
+                                                          new Date(
+                                                              value.end_date
+                                                          ),
+                                                          "MMMM yyyy"
+                                                      )}
+                                                  </p>
+                                                  <p className="address">
+                                                      {value.location}
+                                                  </p>
+                                              </div>
+                                          )
+                                      )
+                                    : "Looks like you have no Education Data"}
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className="type certification">
+                    <div className="title-wrapper d-flex justify-content-between">
+                        {/* <h2 className="heading-title">Community activity</h2> */}
+                        <h1>Certifications</h1>
+                    </div>
+                    <Row>
+                        <Col md={9}>
+                            <div className="content">
+                                {certificationData
+                                    ? certificationData?.data.result?.map(
+                                          (value, key) => (
+                                              <div
+                                                  className="certification__type"
+                                                  key={key}
+                                              >
+                                                  <div className="name d-flex">
+                                                      {/* <Link
+                                                          href={
+                                                              value?.certificate_url
+                                                          }
+                                                      > */}
+                                                      <a
+                                                          href={
+                                                              value?.certificate_url
+                                                          }
+                                                          target="_blank"
+                                                          rel="noreferrer"
+                                                      >
+                                                          <h3 className="institution">
+                                                              {value?.name}
+                                                          </h3>
+                                                      </a>
+                                                      {/* </Link> */}
+                                                  </div>
+                                                  <h3 className="program">
+                                                      {value?.description}
+                                                  </h3>
+                                                  <p className="date">
+                                                      {format(
+                                                          new Date(
+                                                              value?.issued_date
+                                                          ),
+                                                          "MMMM yyyy"
+                                                      )}
+                                                      {`${
+                                                          value?.expire_date
+                                                              ? `-`
+                                                              : "- Present"
+                                                      }`}
+                                                      {value?.expire_date &&
+                                                          format(
+                                                              new Date(
+                                                                  value.expire_date
+                                                              ),
+                                                              "MMMM yyyy"
+                                                          )}
+                                                  </p>
+                                              </div>
+                                          )
+                                      )
+                                    : "Looks like you have no certificates"}
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        </>
+    );
+};
