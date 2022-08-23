162cc8527768909c1e46ba7e0c53a047df6f4a4f
Sandip <sandipthapa@cagtu.com>
2022-08-23 15:56:26 +0545

    Added layout to the earn-money page

--- pages/earn-money.tsx
+++ pages/earn-money.tsx
@@ -1,13 +1,16 @@
 import { BreadCrumb } from "@components/common/BreadCrumb";
 import { EarnMoneyProfessional } from "@components/EarnMoney/EarnMoneyProfessional";
+import Layout from "@components/Layout";
 import React from "react";
 
 const EarnMoney = () => {
     return (
-        <div className="earn-money-page">
-            <BreadCrumb currentPage={"Earn money as professional"} />
-            <EarnMoneyProfessional />
-        </div>
+        <Layout title="Earn Money | Cipher">
+            <div className="earn-money-page">
+                <BreadCrumb currentPage={"Earn money as professional"} />
+                <EarnMoneyProfessional />
+            </div>
+        </Layout>
     );
 };
 
