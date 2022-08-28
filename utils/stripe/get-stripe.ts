5e85ae82cab01477aa9d182f729141d1fa97bc1c
Sandip <sandipthapa@cagtu.com>
2022-08-28 10:31:14 +0545

    File added to create the Stripe instance

---
+++ utils/stripe/get-stripe.ts
@@ -0,0 +1,14 @@
+import type { Stripe } from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+let stripePromise: Promise<Stripe | null>;
+const getStripe = () => {
+    if (!stripePromise) {
+        stripePromise = loadStripe(
+            process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+        );
+    }
+    return stripePromise;
+};
+
+export default getStripe;
