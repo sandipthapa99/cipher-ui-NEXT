25e29a607df914de150f8a46506b396163141a46
Samikshya <samikshyabasnet@cagtu.com>
2022-09-05 10:07:02 +0545

    profile ellipsis dropdown content added

--- components/common/EllipsisDropdown.tsx
+++ components/common/EllipsisDropdown.tsx
@@ -1,52 +1,111 @@
 import {
     faEyeSlash,
+    faGear,
     faPencil,
     faTrashCan,
 } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 import type { ReactNode } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
-
+interface menu {
+    item: string;
+    onClick: string;
+    redirectTo?: string;
+}
 interface DropdownProps {
     children?: ReactNode;
     showModal?: boolean;
+    menu: menu[];
     handleOnClick?: () => void;
 }
-
-const EllipsisDropdown = ({ children, handleOnClick }: DropdownProps) => {
+const EllipsisDropdown = ({ children, handleOnClick, menu }: DropdownProps) => {
     return (
         <div className="ellipsis">
             <Dropdown>
                 <Dropdown.Toggle>{children && <>{children}</>}</Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item
-                        href="#/action-1"
-                        onClick={handleOnClick}
-                        className="d-flex align-items-center"
-                    >
-                        <FontAwesomeIcon className="svg-icon" icon={faPencil} />
-                        Edit
-                    </Dropdown.Item>
-                    <Dropdown.Item
-                        href="#/action-2"
-                        className="d-flex align-items-center"
-                    >
-                        <FontAwesomeIcon
-                            className="svg-icon"
-                            icon={faTrashCan}
-                        />
-                        Remove
-                    </Dropdown.Item>
-                    <Dropdown.Item
-                        href="#/action-3"
-                        className="d-flex align-items-center"
-                    >
-                        <FontAwesomeIcon
-                            className="svg-icon"
-                            icon={faEyeSlash}
-                        />
-                        Inactive
-                    </Dropdown.Item>
+                    {menu ? (
+                        menu.map((item, i) => (
+                            <Dropdown.Item
+                                key={i}
+                                href="#/action-1"
+                                onClick={handleOnClick}
+                                className="d-flex align-items-center"
+                            >
+                                {item.redirectTo ? (
+                                    <Link href={`/${item.redirectTo}`}>
+                                        <a>
+                                            <FontAwesomeIcon
+                                                className="svg-icon"
+                                                icon={
+                                                    item.item === "Edit"
+                                                        ? faPencil
+                                                        : item.item ===
+                                                          "Settings"
+                                                        ? faGear
+                                                        : item.item === "Delete"
+                                                        ? faTrashCan
+                                                        : faEyeSlash
+                                                }
+                                            />
+                                            {item.item}
+                                        </a>
+                                    </Link>
+                                ) : (
+                                    <div>
+                                        <FontAwesomeIcon
+                                            className="svg-icon"
+                                            icon={
+                                                item.item === "Edit"
+                                                    ? faPencil
+                                                    : item.item === "Settings"
+                                                    ? faGear
+                                                    : item.item === "Delete"
+                                                    ? faTrashCan
+                                                    : faEyeSlash
+                                            }
+                                        />
+                                        {item.item}
+                                    </div>
+                                )}
+                            </Dropdown.Item>
+                        ))
+                    ) : (
+                        <>
+                            <Dropdown.Item
+                                href="#/action-1"
+                                onClick={handleOnClick}
+                                className="d-flex align-items-center"
+                            >
+                                <FontAwesomeIcon
+                                    className="svg-icon"
+                                    icon={faPencil}
+                                />
+                                Edit
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                href="#/action-2"
+                                className="d-flex align-items-center"
+                            >
+                                <FontAwesomeIcon
+                                    className="svg-icon"
+                                    icon={faTrashCan}
+                                />
+                                Remove
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                href="#/action-3"
+                                className="d-flex align-items-center"
+                            >
+                                <FontAwesomeIcon
+                                    className="svg-icon"
+                                    icon={faEyeSlash}
+                                />
+                                Inactive
+                            </Dropdown.Item>
+                        </>
+                    )}
                 </Dropdown.Menu>
             </Dropdown>
         </div>
