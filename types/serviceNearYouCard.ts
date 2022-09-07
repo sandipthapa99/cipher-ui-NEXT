fc72a156b766af55da4888acc0470c38806524ce
Subash Poudel <subashpoudel@cagtu.com>
2022-09-07 15:32:11 +0545

    type error fixed

--- types/serviceNearYouCard.ts
+++ types/serviceNearYouCard.ts
@@ -12,7 +12,7 @@ export interface ServiceNearYouCardProps {
               placeholder: string;
           }[];
     serviceTitle?: string;
-    currency: string;
+    currency?: string;
     serviceProvider?: string;
     serviceProviderLocation?: string;
     serviceRating?: string | number;
