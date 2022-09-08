d709e1c9873dd3a22ca2358f7071d0a3911e2228
Subash Poudel <subashpoudel@cagtu.com>
2022-09-08 12:11:24 +0545

    api integration of my tasks home

--- components/MyTasks/MyTasks.tsx
+++ components/MyTasks/MyTasks.tsx
@@ -1,17 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import type { MyTaskProps } from "types/myTasksProps";
+import { axiosClient } from "utils/axiosClient";
 
 import { MyTaskOrder } from "./MyTaskOrder";
 
 export const MyTasks = () => {
+    const { data: mytaskData } = useQuery(["my-task"], async () => {
+        const response = await axiosClient.get("/task/my-task/");
+        return response.data.result;
+    });
+    
     return (
         <div className="my-task">
             <h3>My Tasks</h3>
 
             <div className="my-task__each-orders">
-                <MyTaskOrder />
-                <MyTaskOrder />
-                <MyTaskOrder />
-                <MyTaskOrder />
+                {mytaskData &&
+                    mytaskData?.map((item: MyTaskProps, index: number) => (
+                        <MyTaskOrder key={index} myTask={item} />
+                    ))}
             </div>
         </div>
     );
