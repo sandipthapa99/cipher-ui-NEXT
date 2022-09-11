50a298515a17e6d15d00438acd52030b8b8a9b3d
Sandip <sandipthapa@cagtu.com>
2022-09-11 16:53:10 +0545

    (Bugfix) Added password validation in password change field

--- utils/formValidation/changePasswordFormValidation.ts
+++ utils/formValidation/changePasswordFormValidation.ts
@@ -3,7 +3,10 @@ import * as Yup from "yup";
 const passwordValidate = Yup.string()
     .required("Required field")
     .min(8, "Password is too short - should be 8 chars minimum.")
-    .matches(/[a-zA-Z]/, "Password can only contain Latin letters.");
+    .matches(
+        /^(?=.*[0-9])(?=.*[!@#$%^&*+-])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*+-]{6,16}$/,
+        "Password must contain 1 numberic value, 1 uppercase and 1 special character."
+    );
 
 const changePasswordFormSchema = Yup.object().shape({
     old_password: passwordValidate,
