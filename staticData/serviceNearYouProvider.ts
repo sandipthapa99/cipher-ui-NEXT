97701c138c08b38f118a68705fd96f261266fcc8
darshanman <darshanman@cagtu.com>
2022-09-14 10:40:15 +0545

    Revert "refact(serviceNearYouProvder) static data removed"
    This reverts commit f973ede2d90feec962ca1cc4f8685f54e69da150.

---
+++ staticData/serviceNearYouProvider.ts
@@ -0,0 +1,9 @@
+export const ServiceNearYouProvider = [
+    {
+        id: "0",
+        name: "Harry Smith",
+        speciality: "Gardener",
+        price: 1000,
+        image: "/service-details/provider1.svg",
+    },
+];
