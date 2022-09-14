ab38644e3651cbcf86e136b86d458e9d801893dc
darshanman <darshanman@cagtu.com>
2022-09-14 10:58:18 +0545

    refact(services) static data removed

--- services/commonServices.ts
+++ services/commonServices.ts
@@ -1,6 +1,5 @@
 import { reviewsContent } from "staticData/reviews";
 import { SearchBodyData } from "staticData/searchBody";
-import { services } from "staticData/services";
 import { axiosClient } from "utils/axiosClient";
 
 //Get-------------------
@@ -15,11 +14,6 @@ export const getSearchBody = () => {
     // const response = await axiosClient.get("/searchBody");
     // return response.data;
     return SearchBodyData;
-};
-export const getServices = () => {
-    // const response = await axiosClient.get("/services");
-    // return response.data;
-    return services;
 };
 export const getReviews = () => {
     // const response = await axiosClient.get("/reviews");
