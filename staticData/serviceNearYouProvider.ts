404228c89d0e0f5a0423a8a5a2512dbd4afb0a40
darshanman <darshanman@cagtu.com>
2022-09-14 10:44:08 +0545

    refact(serviceNearYouProvder) static data removed

--- staticData/serviceNearYouProvider.ts
+++
@@ -1,9 +0,0 @@
-export const ServiceNearYouProvider = [
-    {
-        id: "0",
-        name: "Harry Smith",
-        speciality: "Gardener",
-        price: 1000,
-        image: "/service-details/provider1.svg",
-    },
-];
