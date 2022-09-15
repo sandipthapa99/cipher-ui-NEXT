a121a70ed4bb9c880198e26a41c0102c96f1f6a1
Samikshya <samikshyabasnet@cagtu.com>
2022-09-15 17:58:03 +0545

    kyc status updated

--- components/settings/KycStatus.tsx
+++ components/settings/KycStatus.tsx
@@ -8,40 +8,47 @@ import {
 } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Accordion, Alert, Badge, Container, Divider } from "@mantine/core";
+import { format } from "date-fns";
 import { useGetKYC } from "hooks/profile/kyc/useGetKYC";
 import { useGetProfile } from "hooks/profile/useGetProfile";
 import { useData } from "hooks/use-data";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
+
+interface KYCDocumentType {
+    id: number;
+    created_at: string;
+    updated_at: string;
+    document_type: string;
+    document_id: string;
+    file: string;
+    issuer_organization: string;
+    issued_date: string;
+    valid_through: any;
+    is_verified: boolean;
+    is_company: boolean;
+    comment: any;
+}
 
 export const KYCStatus = () => {
     const { data: profileDetails } = useGetProfile();
     const { data: KycData } = useGetKYC();
-
-    const { data: KycDocuments } = useGetKYCDocument();
-
-    // KYCData.then((res) => console.log(res));
-
+    const { data: KycDocuments } = useData<KYCDocumentType[]>(
+        ["tasker-document"],
+        "/tasker/kyc-document"
+    );
     console.log("KycDocuments", KycDocuments);
-
-    const renderDocuments = KycDocuments?.map((item, index: number) => {
+    console.log("KycData", KycData);
+    const renderDocuments = KycDocuments?.data?.map((item, index: number) => {
         return (
-            <Accordion.Item key={index} value={item?.id.toString()}>
+            <Accordion.Item key={index} value={item?.document_id}>
                 <Accordion.Control className="mt-1 p-3">
                     <div className="d-flex align-items-center gap-3 folder-text-accordian">
                         <FontAwesomeIcon icon={faFolder} />
-                        <p className="m-0 document-kyc">
-                            {item?.document_type}
-                        </p>
-                        {item?.is_verified && (
-                            <div className="badge-icon-kyc-verified">
-                                <FontAwesomeIcon
-                                    icon={faCircleCheck}
-                                    className="badge-icon"
-                                />
-                            </div>
-                        )}
+                        <p className="m-0 document-kyc">Documents</p>
+                        <Badge color="green">
+                            <FontAwesomeIcon icon={faCircleCheck} />
+                        </Badge>
                     </div>
                 </Accordion.Control>
                 <Accordion.Panel>
@@ -69,7 +76,7 @@ export const KYCStatus = () => {
                         <Col className="Kyc-documnet-verify" md={3}>
                             <p className="m-0 document-text">Expiry Date</p>
                             <p className="m-0 document-text-value">
-                                {item.issued_date}
+                                {item.valid_through}
                             </p>
                         </Col>
                     </Row>
@@ -107,7 +114,7 @@ export const KYCStatus = () => {
                         className="photo-kyc-status"
                     />
                     <div className="text-cont-kyc">
-                        <p className="m-0 title-kyc">{}</p>
+                        <p className="m-0 title-kyc">{KycData?.full_name}</p>
                         <p className="m-0 body-kyc">
                             @{profileDetails?.user.full_name}
                         </p>
@@ -128,9 +135,7 @@ export const KYCStatus = () => {
                                 className="font-icon-kyc"
                             />
                             <p className="m-0 body-kyc">
-                                {profileDetails?.user?.phone
-                                    ? profileDetails?.user?.phone
-                                    : "Add a Phone Number"}
+                                {profileDetails?.user?.phone}
                             </p>
                         </div>
                     </div>
@@ -141,15 +146,17 @@ export const KYCStatus = () => {
                             icon={faLocationDot}
                             className="font-icon-kyc text-black"
                         />
-                        <p className="m-0 body-kyc">{KycData?.country?.name}</p>
+                        <p className="m-0 body-kyc">{KycData?.country.name}</p>
                     </div>
                     <p className="m-0 body-kyc">
                         <span className="body-kyc-span">Submitted On: </span>{" "}
-                        {format(new Date(KycData?.created_at), "do MMM yyyy")}
+                        {/* {format(new Date(), "yyyy-MM-dd")} */}
+                        2022-20-20
                     </p>
                     <p className="m-0 body-kyc">
                         <span className="body-kyc-span">Updated On: </span>{" "}
-                        {format(new Date(KycData?.updated_at), "do MMM yyyy")}
+                        {/* {format(new Date(), "yyyy-MM-dd")} */}
+                        2022-20-20
                     </p>
                 </Col>
             </Row>
@@ -165,27 +172,21 @@ export const KYCStatus = () => {
                 </Col>
                 <Col className="basic-info-cont" md={3}>
                     <p className="m-0 text-title-basic-info">Kyc Verified</p>
-                    {KycData?.is_kyc_verified ? (
-                        <p className="m-0 text-yes">Yes</p>
-                    ) : (
-                        <p className="m-0 text-pending">Pending</p>
-                    )}
+                    {/* <p className="m-0 text-yes">Yes</p> */}
+                    <p className="m-0 text-pending">Pending</p>
                 </Col>
                 <Col className="basic-info-cont" md={3}>
                     <p className="m-0 text-title-basic-info">
                         Address Verified
                     </p>
-                    {KycData?.is_address_verified ? (
-                        <p className="m-0 text-yes">Yes</p>
-                    ) : (
-                        <p className="m-0 text-pending">Pending</p>
-                    )}
+                    {/* <p className="m-0 text-yes">Yes</p> */}
+                    <p className="m-0 text-pending">Pending</p>
                 </Col>
             </Row>
             <Row className="">
                 <h2 className="mt-5 title-kyc-status">KYC Documents</h2>
                 <Divider my="sm" size="xs" variant="dashed" className="mb-4" />
-                {KycDocuments?.length === 0 ? (
+                {!KycData && KycDocuments?.data?.length === 0 ? (
                     <Alert
                         icon={<FontAwesomeIcon icon={faCircleQuestion} />}
                         title=""
