1d90b304786a23d58597f0623e9a244d035f4819
Sundar Gautam <sundargautam@cagtu.com>
2022-09-16 15:01:58 +0545

    category-icon:added

--- components/common/Dropdown.tsx
+++ components/common/Dropdown.tsx
@@ -1,5 +1,6 @@
 import { faChevronRight } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import parse from "html-react-parser";
 import Link from "next/link";
 import type { ReactNode } from "react";
 import { useState } from "react";
@@ -99,8 +100,20 @@ export const Dropdown = ({ children }: DropdownProps) => {
                                 setIsSubMenuOpened(false);
                                 setIsNestedSubMenuOpened(false);
                             }}
-                            className="dropdown-menu-item-link"
+                            className="dropdown-menu-item-link d-flex gap-3"
                         >
+                            {" "}
+                            <div className="image-wrapper">
+                                <figure className="d-flex align-items-center justify-content-center thumbnail-icon">
+                                    {sub?.icon
+                                        ? parse(sub?.icon)
+                                        : parse(
+                                              `<svg width="464" height="464" viewBox="0 0 464 464" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M144 0C170.5 0 192 21.49 192 48V144C192 170.5 170.5 192 144 192H48C21.49 192 0 170.5 0 144V48C0 21.49 21.49 0 48 0H144ZM144 48H48V144H144V48ZM144 256C170.5 256 192 277.5 192 304V400C192 426.5 170.5 448 144 448H48C21.49 448 0 426.5 0 400V304C0 277.5 21.49 256 48 256H144ZM144 304H48V400H144V304ZM256 48C256 21.49 277.5 0 304 0H400C426.5 0 448 21.49 448 48V144C448 170.5 426.5 192 400 192H304C277.5 192 256 170.5 256 144V48ZM304 144H400V48H304V144ZM352 240C365.3 240 376 250.7 376 264V328H440C453.3 328 464 338.7 464 352C464 365.3 453.3 376 440 376H376V440C376 453.3 365.3 464 352 464C338.7 464 328 453.3 328 440V376H264C250.7 376 240 365.3 240 352C240 338.7 250.7 328 264 328H328V264C328 250.7 338.7 240 352 240Z" fill="white"/>
+                      </svg>`
+                                          )}
+                                </figure>
+                            </div>
                             {`${sub.name}`}{" "}
                         </a>
                     </Link>
@@ -116,8 +129,20 @@ export const Dropdown = ({ children }: DropdownProps) => {
                             setIsSubMenuOpened(false);
                             setIsNestedSubMenuOpened(false);
                         }}
-                        className="dropdown-menu-item-link"
+                        className="dropdown-menu-item-link d-flex gap-3"
                     >
+                        {" "}
+                        <div className="image-wrapper">
+                            <figure className="d-flex align-items-center justify-content-center thumbnail-icon">
+                                {sub?.icon
+                                    ? parse(sub?.icon)
+                                    : parse(
+                                          `<svg width="464" height="464" viewBox="0 0 464 464" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M144 0C170.5 0 192 21.49 192 48V144C192 170.5 170.5 192 144 192H48C21.49 192 0 170.5 0 144V48C0 21.49 21.49 0 48 0H144ZM144 48H48V144H144V48ZM144 256C170.5 256 192 277.5 192 304V400C192 426.5 170.5 448 144 448H48C21.49 448 0 426.5 0 400V304C0 277.5 21.49 256 48 256H144ZM144 304H48V400H144V304ZM256 48C256 21.49 277.5 0 304 0H400C426.5 0 448 21.49 448 48V144C448 170.5 426.5 192 400 192H304C277.5 192 256 170.5 256 144V48ZM304 144H400V48H304V144ZM352 240C365.3 240 376 250.7 376 264V328H440C453.3 328 464 338.7 464 352C464 365.3 453.3 376 440 376H376V440C376 453.3 365.3 464 352 464C338.7 464 328 453.3 328 440V376H264C250.7 376 240 365.3 240 352C240 338.7 250.7 328 264 328H328V264C328 250.7 338.7 240 352 240Z" fill="white"/>
+                  </svg>`
+                                      )}
+                            </figure>
+                        </div>
                         {`${sub.name}`}{" "}
                     </a>
                 </Link>
@@ -154,8 +179,20 @@ export const Dropdown = ({ children }: DropdownProps) => {
                                 setIsNestedSubMenuOpened(false);
                             }}
                             onMouseOver={onHandleDropdown}
-                            className="dropdown-menu-item-link"
+                            className="dropdown-menu-item-link d-flex gap-3"
                         >
+                            {" "}
+                            <div className="image-wrapper">
+                                <figure className="d-flex align-items-center justify-content-center thumbnail-icon">
+                                    {sub?.icon
+                                        ? parse(sub?.icon)
+                                        : parse(
+                                              `<svg width="464" height="464" viewBox="0 0 464 464" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M144 0C170.5 0 192 21.49 192 48V144C192 170.5 170.5 192 144 192H48C21.49 192 0 170.5 0 144V48C0 21.49 21.49 0 48 0H144ZM144 48H48V144H144V48ZM144 256C170.5 256 192 277.5 192 304V400C192 426.5 170.5 448 144 448H48C21.49 448 0 426.5 0 400V304C0 277.5 21.49 256 48 256H144ZM144 304H48V400H144V304ZM256 48C256 21.49 277.5 0 304 0H400C426.5 0 448 21.49 448 48V144C448 170.5 426.5 192 400 192H304C277.5 192 256 170.5 256 144V48ZM304 144H400V48H304V144ZM352 240C365.3 240 376 250.7 376 264V328H440C453.3 328 464 338.7 464 352C464 365.3 453.3 376 440 376H376V440C376 453.3 365.3 464 352 464C338.7 464 328 453.3 328 440V376H264C250.7 376 240 365.3 240 352C240 338.7 250.7 328 264 328H328V264C328 250.7 338.7 240 352 240Z" fill="white"/>
+                      </svg>`
+                                          )}
+                                </figure>
+                            </div>
                             {`${sub.name}`}{" "}
                             {sub?.child?.length > 0
                                 ? `(${sub?.child?.length})`
@@ -178,8 +215,20 @@ export const Dropdown = ({ children }: DropdownProps) => {
                             setIsSubMenuOpened(false);
                             setIsNestedSubMenuOpened(false);
                         }}
-                        className="dropdown-menu-item-link"
+                        className="dropdown-menu-item-link d-flex gap-3"
                     >
+                        {" "}
+                        <div className="image-wrapper">
+                            <figure className="d-flex align-items-center justify-content-center thumbnail-icon">
+                                {sub?.icon
+                                    ? parse(sub?.icon)
+                                    : parse(
+                                          `<svg width="464" height="464" viewBox="0 0 464 464" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M144 0C170.5 0 192 21.49 192 48V144C192 170.5 170.5 192 144 192H48C21.49 192 0 170.5 0 144V48C0 21.49 21.49 0 48 0H144ZM144 48H48V144H144V48ZM144 256C170.5 256 192 277.5 192 304V400C192 426.5 170.5 448 144 448H48C21.49 448 0 426.5 0 400V304C0 277.5 21.49 256 48 256H144ZM144 304H48V400H144V304ZM256 48C256 21.49 277.5 0 304 0H400C426.5 0 448 21.49 448 48V144C448 170.5 426.5 192 400 192H304C277.5 192 256 170.5 256 144V48ZM304 144H400V48H304V144ZM352 240C365.3 240 376 250.7 376 264V328H440C453.3 328 464 338.7 464 352C464 365.3 453.3 376 440 376H376V440C376 453.3 365.3 464 352 464C338.7 464 328 453.3 328 440V376H264C250.7 376 240 365.3 240 352C240 338.7 250.7 328 264 328H328V264C328 250.7 338.7 240 352 240Z" fill="white"/>
+                          </svg>`
+                                      )}
+                            </figure>
+                        </div>
                         {`${sub.name}`}
                         {sub?.child?.length > 0
                             ? `(${sub?.child?.length})`
@@ -218,8 +267,19 @@ export const Dropdown = ({ children }: DropdownProps) => {
                             setIsSubMenuOpened(false);
                             setIsNestedSubMenuOpened(false);
                         }}
-                        className="dropdown-menu-item-link"
+                        className="dropdown-menu-item-link d-flex gap-3"
                     >
+                        <div className="image-wrapper">
+                            <figure className="d-flex align-items-center justify-content-center thumbnail-icon">
+                                {item?.icon
+                                    ? parse(item?.icon)
+                                    : parse(
+                                          `<svg width="464" height="464" viewBox="0 0 464 464" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M144 0C170.5 0 192 21.49 192 48V144C192 170.5 170.5 192 144 192H48C21.49 192 0 170.5 0 144V48C0 21.49 21.49 0 48 0H144ZM144 48H48V144H144V48ZM144 256C170.5 256 192 277.5 192 304V400C192 426.5 170.5 448 144 448H48C21.49 448 0 426.5 0 400V304C0 277.5 21.49 256 48 256H144ZM144 304H48V400H144V304ZM256 48C256 21.49 277.5 0 304 0H400C426.5 0 448 21.49 448 48V144C448 170.5 426.5 192 400 192H304C277.5 192 256 170.5 256 144V48ZM304 144H400V48H304V144ZM352 240C365.3 240 376 250.7 376 264V328H440C453.3 328 464 338.7 464 352C464 365.3 453.3 376 440 376H376V440C376 453.3 365.3 464 352 464C338.7 464 328 453.3 328 440V376H264C250.7 376 240 365.3 240 352C240 338.7 250.7 328 264 328H328V264C328 250.7 338.7 240 352 240Z" fill="white"/>
+                                  </svg>`
+                                      )}
+                            </figure>
+                        </div>
                         {item.name}
                     </a>
                 </Link>
@@ -248,7 +308,12 @@ export const Dropdown = ({ children }: DropdownProps) => {
                         {/*View All  */}
                         <li className="dropdown-menu-items d-flex justify-space-between">
                             <Link href="/category">
-                                <a className="dropdown-menu-item-link fw-bold">
+                                <a
+                                    className="dropdown-menu-item-link fw-2"
+                                    style={{
+                                        color: "#0693E3",
+                                    }}
+                                >
                                     View All Category
                                 </a>
                             </Link>
