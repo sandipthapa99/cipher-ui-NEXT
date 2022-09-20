44133280fb1dc74b24c3360fd6395d9ecd386ee9
Sundar Gautam <sundargautam@cagtu.com>
2022-09-20 11:36:37 +0545

    profile modal:fix and offer page added

--- pages/offers/index.tsx
+++ pages/offers/index.tsx
@@ -1,5 +1,6 @@
 import { BreadCrumb } from "@components/common/BreadCrumb";
 import Layout from "@components/Layout";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
 import { Container, Row } from "react-bootstrap";
@@ -9,9 +10,28 @@ const Offers = () => {
 
     return (
         <Layout title="Cipher -offers">
-            <section className="recommend">
+            <section className="recommend offers">
                 <Container fluid="xl" className="px-5">
                     <BreadCrumb currentPage={"Offers"} />
+                    <div className="offer-wrapper d-flex gap-5">
+                        <figure className="offer-img">
+                            <Image
+                                src={"/discountcard1.png"}
+                                height={400}
+                                width={648}
+                                alt={"voucher"}
+                                objectFit="contain"
+                            />
+                        </figure>
+                        <figure className="offer-img">
+                            <Image
+                                src={"/discountcard2.png"}
+                                height={400}
+                                width={648}
+                                alt={"voucher"}
+                            />
+                        </figure>
+                    </div>
                     <h2>Recommend</h2>
                 </Container>
             </section>
