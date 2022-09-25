6216033168a0c161a63e4f95012abe517b989b0c
Sachin Aryal <sachinaryal@cagtu.com>
2022-09-25 16:11:40 +0545

    Updated loginPrompt with login function

--- store/use-login-prompt-store.ts
+++ store/use-login-prompt-store.ts
@@ -4,12 +4,14 @@ import { createStore, useStore } from "zustand";
 type CustomFunction = () => void;
 
 interface LoginPromptStore {
+    pausedFunction?: any;
     showPrompt: boolean;
     hidePrompt: () => void;
     withLogin: (
         _function: CustomFunction,
         notLoggedInFunction?: CustomFunction
     ) => CustomFunction;
+    clearPausedFunction: () => void;
 }
 
 export const loginPromptStore = createStore<LoginPromptStore>((set) => {
@@ -17,15 +19,25 @@ export const loginPromptStore = createStore<LoginPromptStore>((set) => {
         set((state) => ({ ...state, showPrompt: true }));
 
     return {
+        pausedFunction: undefined,
         showPrompt: false,
         hidePrompt: () => set((state) => ({ ...state, showPrompt: false })),
         withLogin: (_function, notLoggedInFunction) => {
             //! temporary workground untill zustand@4 releases proper docs
             const user = Cookies.get("access");
+            if (!user) {
+                set((state) => ({ ...state, pausedFunction: _function }));
+            }
             return user ? _function : notLoggedInFunction ?? showLoginPrompt;
         },
+        clearPausedFunction: () =>
+            set((state) => ({ ...state, pausedFunction: undefined })),
     };
 });
 export const useShowLoginPrompt = () => useStore(loginPromptStore).showPrompt;
 export const useHideLoginPrompt = () => useStore(loginPromptStore).hidePrompt;
 export const useWithLogin = () => useStore(loginPromptStore).withLogin;
+export const usePausedFunction = () =>
+    useStore(loginPromptStore).pausedFunction;
+export const useClearPausedFunction = () =>
+    useStore(loginPromptStore).clearPausedFunction;
