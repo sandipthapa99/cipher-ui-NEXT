f994950002e5748434610b9ba9edeb747194cb12
Sachin Aryal <sachinaryal@cagtu.com>
2022-09-25 11:50:02 +0545

    Added rich text editor on post service modal

--- components/RichText.tsx
+++ components/RichText.tsx
@@ -4,7 +4,7 @@ import type { RichTextEditorProps } from "@mantine/rte";
 export const RichText = (props: RichTextEditorProps) => {
     if (typeof window !== "undefined") {
         const { RichTextEditor } = require("@mantine/rte");
-        return <RichTextEditor {...props} />;
+        return <RichTextEditor sx={{ height: "250px" }} {...props} />;
     }
     return null;
 };
