2b5cf16eba3c1c04f27edc1a94d288843faabf56
Samikshya <samikshyabasnet@cagtu.com>
2022-09-28 09:58:14 +0545

    working on bank form display feature

--- components/settings/AddBankForm.tsx
+++ components/settings/AddBankForm.tsx
@@ -5,10 +5,11 @@ import { Select } from "@mantine/core";
 import { useQueryClient } from "@tanstack/react-query";
 import { Field, Form, Formik } from "formik";
 import { useGetKYC } from "hooks/profile/kyc/useGetKYC";
+import { useGetProfile } from "hooks/profile/useGetProfile";
 import { useData } from "hooks/use-data";
 import { useEditForm } from "hooks/use-edit-form";
 import { useForm } from "hooks/use-form";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { toast } from "react-toastify";
 import type {
@@ -20,12 +21,17 @@ import type {
 import { bankFormSchema } from "utils/formValidation/bankDetailsValidation";
 import { isSubmittingClass } from "utils/helpers";
 
+import { CompleteProfile } from "./ProfileForm";
+
 interface editProps {
     id?: number;
     bankDetail?: any;
     isEdit?: boolean;
 }
 const BankForm = ({ id, isEdit, bankDetail }: editProps) => {
+    const [showBankForm, setShowBankForm] = useState(false);
+    const [showCompleteProfile, setShowCompleteProfile] = useState(true);
+
     const { mutate } = useForm(`/tasker/bank-details/`);
 
     const [bankId, setBankId] = useState<string>(
@@ -123,9 +129,24 @@ const BankForm = ({ id, isEdit, bankDetail }: editProps) => {
 
     const accname = editDetails?.bank_account_name;
     const accnumber = editDetails?.bank_account_number;
+    const { data: profile } = useGetProfile();
 
+    const ref = useRef<HTMLDivElement>(null);
+
+    const handleClick = () => {
+        // ref.current?.scrollIntoView({ behavior: "smooth" });
+        setShowBankForm(true);
+    };
     return (
-        <div className="bank-form">
+        <div
+            //ref={ref}
+            className="bank-form"
+            style={
+                showBankForm || profile
+                    ? { display: "block" }
+                    : { display: "none" }
+            }
+        >
             <Formik
                 enableReinitialize={true}
                 initialValues={
@@ -305,6 +326,7 @@ const BankForm = ({ id, isEdit, bankDetail }: editProps) => {
                     </Form>
                 )}
             </Formik>
+            {LinkedBank ? <CompleteProfile onClick={handleClick} /> : ""}
         </div>
     );
 };
