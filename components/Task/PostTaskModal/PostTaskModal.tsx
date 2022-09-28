f673523967a847a51bf23d6eb39afc20f34b77c6
Sachin Aryal <sachinaryal@cagtu.com>
2022-09-28 10:10:15 +0545

    Remove stringify highlights on addservicemodal

--- components/Task/PostTaskModal/PostTaskModal.tsx
+++ components/Task/PostTaskModal/PostTaskModal.tsx
@@ -165,7 +165,7 @@ export const PostTaskModal = () => {
 
             const postTaskPayload = {
                 ...values,
-                highlights: values.highlights),
+                highlights: values.highlights,
                 images: imageIds,
                 videos: videoIds,
                 extra_data: [],
