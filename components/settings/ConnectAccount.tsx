d7e889a07422d5a7fc6538cb2bae38ccac1fbbde
Samikshya <samikshyabasnet@cagtu.com>
2022-10-19 15:28:44 +0545

    account unlink api integration completed

--- components/settings/ConnectAccount.tsx
+++ components/settings/ConnectAccount.tsx
@@ -2,14 +2,44 @@ import BigButton from "@components/common/Button";
 import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { faTrashCan } from "@fortawesome/pro-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useData } from "hooks/use-data";
-import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { useQuery } from "@tanstack/react-query";
+import { log } from "console";
+import { useGoogle } from "hooks/auth/use-Google";
+import Cookies from "js-cookie";
+import localforage from "localforage";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { autoLogin } from "utils/auth";
+import { axiosClient } from "utils/axiosClient";
+import { toast } from "utils/toast";
 
 interface AccountInfo {
     name: string;
+    disconnect?: boolean;
 }
 
-const ConnectAccount = ({ name }: AccountInfo) => {
+const ConnectAccount = ({ name, disconnect }: AccountInfo) => {
+    // const login = () => {};
+    const { mutate, data } = useGoogle();
+    const [FCM_TOKEN, setFCM_TOKEN] = useState("");
+    const router = useRouter();
+    const getFCMTOKEN = async () => {
+        if (typeof window !== "undefined") {
+            const token = await localforage.getItem<string>("fcm_token");
+            return token;
+        }
+        return null;
+    };
+    const token = getFCMTOKEN();
+    token.then((token) => {
+        if (token) {
+            setFCM_TOKEN(token);
+        }
+    });
+    const disconnectAccount = () => {
+        console.log("disconnect");
+    };
     return (
         <div className="account-wrapper">
             <div className="social-connection  me-4">
@@ -25,11 +55,45 @@ const ConnectAccount = ({ name }: AccountInfo) => {
                 {/* </span> */}
                 <p>You are not signed in through {name}.</p>
                 <div className="d-flex button-wrapper align-items-stretch justify-content-center my-4">
+                    {/* <GoogleLogin
+                        size="large"
+                        auto_select={false}
+                        theme="outline"
+                        // width="1200px"
+                        onSuccess={(credentialResponse) => {
+                            const newData = {
+                                ...credentialResponse,
+                                FCM_TOKEN,
+                            };
+
+                            mutate(newData, {
+                                onSuccess: (data) => {
+                                    autoLogin(
+                                        data.access,
+                                        data.refresh,
+                                        credentialResponse.credential
+                                    );
+                                    toast.success("Successfully logged in");
+                                    router.push("/home");
+                                },
+                                onError: (err) => {
+                                    toast.error(err.message);
+                                },
+                            });
+                            //
+                        }}
+                        onError={() => {
+                            toast.error("Error logging in");
+                        }}
+                    /> */}
+
                     <BigButton
                         btnTitle={"Connect"}
                         backgroundColor={"#211d4f"}
                         textColor={"#fff"}
-                        //  handleClick={handleCloseModal}
+                        // handleClick={
+
+                        // }
                     />
                 </div>
             </div>
---
+++ components/settings/PasswordModal.tsx
@@ -0,0 +1,118 @@
+import FormButton from "@components/common/FormButton";
+import InputField from "@components/common/InputField";
+import PasswordField from "@components/common/PasswordField";
+import { async } from "@firebase/util";
+import { useQueryClient } from "@tanstack/react-query";
+import { Form, Formik } from "formik";
+import { useUser } from "hooks/auth/useUser";
+import { useForm } from "hooks/use-form";
+import { useDeleteAccount } from "hooks/use-post-delete";
+import { useRouter } from "next/router";
+import type { Dispatch, SetStateAction } from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { isSubmittingClass } from "utils/helpers";
+import { toast } from "utils/toast";
+import * as Yup from "yup";
+interface PasswordModalCardProps {
+    show?: boolean;
+    handleClose?: () => void;
+    setShowForm: Dispatch<SetStateAction<boolean>>;
+}
+interface AuthProps {
+    id?: number;
+}
+
+const PasswordModalCard = ({
+    handleClose,
+    show,
+    id,
+    setShowForm,
+}: PasswordModalCardProps & AuthProps) => {
+    const stringReqOnly = Yup.string().required("Required field");
+
+    const passwordSchema = Yup.object().shape({
+        password: stringReqOnly,
+    });
+
+    const { mutate } = useDeleteAccount(`/user/unlink/account/${id}/`);
+    const queryClient = useQueryClient();
+    return (
+        <>
+            {/* Modal component */}
+            <Modal
+                show={show}
+                centered
+                onHide={handleClose}
+                className="auth-modal"
+                backdrop="static"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Enter your Password.</Modal.Title>
+                </Modal.Header>
+
+                <div className="modal-body-content">
+                    <Formik
+                        initialValues={{ password: "" }}
+                        validationSchema={passwordSchema}
+                        onSubmit={async (values) => {
+                            mutate(values, {
+                                onSuccess: async (data: any) => {
+                                    toast.success(data.message);
+                                    queryClient.invalidateQueries([
+                                        "linked-accounts",
+                                    ]);
+                                    setShowForm(false);
+                                },
+                                onError: (error: any) => {
+                                    toast.error(
+                                        "Password does not match. Please try again"
+                                    );
+                                    // console.log("hii", error.get("password"));
+                                    // console.log(
+                                    //     Object.values(error.message)[
+                                    //         "password"
+                                    //     ][0]
+                                    // );
+                                },
+                            });
+                        }}
+                    >
+                        {({ isSubmitting, errors, touched }) => (
+                            <Form>
+                                <PasswordField
+                                    name="password"
+                                    labelName="Password"
+                                    touch={touched.password}
+                                    error={errors.password}
+                                    placeHolder="Password"
+                                    fieldRequired
+                                />
+                                <Modal.Footer>
+                                    <Button
+                                        className="btn close-btn"
+                                        onClick={handleClose}
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <FormButton
+                                        type="submit"
+                                        variant="primary"
+                                        name="Submit"
+                                        className="submit-btn"
+                                        isSubmitting={isSubmitting}
+                                        isSubmittingClass={isSubmittingClass(
+                                            isSubmitting
+                                        )}
+                                    />
+                                </Modal.Footer>
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+            </Modal>
+        </>
+    );
+};
+export default PasswordModalCard;
