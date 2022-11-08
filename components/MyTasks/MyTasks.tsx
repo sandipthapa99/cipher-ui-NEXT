7fd462bdd9af9f9ed9fe35b82350b0a310e5090a
subashpoudel1 <subashpoudel@cagtu.com>
2022-11-08 15:29:17 +0545

    my booked task card design in dashboard

--- components/MyTasks/MyTasks.tsx
+++ components/MyTasks/MyTasks.tsx
@@ -1,3 +1,4 @@
+import { MyBookedTaskCard } from "@components/Cards/MyBookedTaskCard";
 import { ApplyPostComponent } from "@components/common/ApplyPostComponent";
 import { Alert, Col, Grid, Loader, Skeleton } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
@@ -27,56 +28,56 @@ export const MyTasks = () => {
     );
 
     return (
-        <div className="my-task">
-            {/* <h3>My Tasks</h3> */}
-
-            <div className="my-task__each-orders">
-                {isLoading ? (
-                    <Grid className="p-5">
-                        <Col span={3}>
-                            <Skeleton height={150} mb="xl" />
-                        </Col>
-                        <Col span={9}>
-                            <Skeleton
-                                height={50}
-                                radius="sm"
-                                className="mb-4"
-                            />
-                            <Skeleton height={50} radius="sm" />
-                        </Col>
-                    </Grid>
-                ) : mytaskData?.length ? (
-                    mytaskData?.map((item: MyTaskProps, index: number) => (
-                        <div className="task-wrapper" key={index}>
-                            <MyTaskOrder
-                                task_id={item?.id}
-                                assigner_id={item?.created_by?.id}
-                                created_at={item?.created_at}
-                                image={item?.images[0]?.media}
-                                title={item?.title}
-                                assigner_name={item?.created_by?.first_name}
-                                budget_from={item?.budget_from}
-                                budget-to={item?.budget_to}
-                                budget_type={item?.budget_type}
-                                status={item?.status}
-                                currency={item?.currency?.symbol}
-                                budget_to={item?.budget_to}
-                                taskID={item?.id}
-                            />
-                        </div>
-                    ))
-                ) : (
-                    <ApplyPostComponent
-                        model="task"
-                        title="No Tasks Available"
-                        subtitle="Post a task to the marketplace and let merchant come to you."
-                        buttonText="Post a Task"
-                    />
-                    // <Alert title="NO DATA AVAILABLE !!!" color="orange">
-                    //     Sorry, You have no task data to show
-                    // </Alert>
-                )}
-            </div>
-        </div>
+        // <div className="my-task">
+        //     <h3>My Tasks</h3>
+        //     <div className="my-task__each-orders">
+        //         {isLoading ? (
+        //             <Grid className="p-5">
+        //                 <Col span={3}>
+        //                     <Skeleton height={150} mb="xl" />
+        //                 </Col>
+        //                 <Col span={9}>
+        //                     <Skeleton
+        //                         height={50}
+        //                         radius="sm"
+        //                         className="mb-4"
+        //                     />
+        //                     <Skeleton height={50} radius="sm" />
+        //                 </Col>
+        //             </Grid>
+        //         ) : mytaskData?.length ? (
+        //             mytaskData?.map((item: MyTaskProps, index: number) => (
+        //                 <div className="task-wrapper" key={index}>
+        //                     <MyTaskOrder
+        //                         task_id={item?.id}
+        //                         assigner_id={item?.created_by?.id}
+        //                         created_at={item?.created_at}
+        //                         image={item?.images[0]?.media}
+        //                         title={item?.title}
+        //                         assigner_name={item?.created_by?.first_name}
+        //                         budget_from={item?.budget_from}
+        //                         budget-to={item?.budget_to}
+        //                         budget_type={item?.budget_type}
+        //                         status={item?.status}
+        //                         currency={item?.currency?.symbol}
+        //                         budget_to={item?.budget_to}
+        //                         taskID={item?.id}
+        //                     />
+        //                 </div>
+        //             ))
+        //         ) : (
+        //             <ApplyPostComponent
+        //                 model="task"
+        //                 title="No Tasks Available"
+        //                 subtitle="Post a task to the marketplace and let merchant come to you."
+        //                 buttonText="Post a Task"
+        //             />
+        //             // <Alert title="NO DATA AVAILABLE !!!" color="orange">
+        //             //     Sorry, You have no task data to show
+        //             // </Alert>
+        //         )}
+        //     </div>
+        // </div>
+        <MyBookedTaskCard />
     );
 };
