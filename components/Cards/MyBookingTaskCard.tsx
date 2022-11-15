cbc31ceaf5e4e2ac6e030b8f8f9345564538c594
Darshan <np03a190140@heraldcollege.edu.np>
2022-11-15 17:21:06 +0545

    (booking-complete) flow added

--- components/Cards/MyBookingTaskCard.tsx
+++ components/Cards/MyBookingTaskCard.tsx
@@ -1,14 +1,18 @@
 import ShareIcon from "@components/common/ShareIcon";
+import { ReviewModal } from "@components/Review/ReviewModal";
 import BookingDetails from "@components/SearchTask/BookingDetails";
 import { faLocationDot } from "@fortawesome/pro-regular-svg-icons";
 import { faHourglassClock, faPeriod } from "@fortawesome/pro-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Badge, RingProgress, Text } from "@mantine/core";
+import { Badge, Button, RingProgress, Text } from "@mantine/core";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { format } from "date-fns";
 import Image from "next/image";
 import React, { useState } from "react";
 import type { ApprovedTaskProps } from "types/approvedTaskProps";
 import type { MyBookingServiceProps } from "types/myBookingProps";
+import { axiosClient } from "utils/axiosClient";
+import { toast } from "utils/toast";
 
 export const MyBookingTaskCard = ({
     item,
@@ -18,6 +22,38 @@ export const MyBookingTaskCard = ({
     Approvedtask?: ApprovedTaskProps["result"][0];
 }) => {
     const [opened, setOpened] = useState(false);
+    const [openReviewModal, setOpenReviewModal] = useState(false);
+
+    const taskCompleteMutaion = useMutation<
+        string,
+        Error,
+        { id: string; status: string }
+    >(async ({ id, status }) =>
+        axiosClient
+            .post("/task/entity/service/task/status/", { task: id, status })
+            .then((res) => res.data.message)
+            .catch((error) => {
+                throw new Error(error?.response?.data);
+            })
+    );
+
+    const queryClient = useQueryClient();
+
+    const taskCompleteHandler = (task_id: string) => {
+        taskCompleteMutaion.mutate(
+            { id: task_id, status: "Completed" },
+            {
+                onSuccess: async (message) => {
+                    await queryClient.invalidateQueries(["approved-task"]);
+                    setOpenReviewModal(true);
+                    toast.success(message);
+                },
+                onError: (error) => {
+                    toast.error(error.message);
+                },
+            }
+        );
+    };
     let status;
     // if (item) {
     //     status = item?.status;
@@ -47,162 +83,201 @@ export const MyBookingTaskCard = ({
         progress = 0;
     }
     return (
-        <div className="my-booking-task-card">
-            <div
-                className="title-price-wrapper d-flex justify-content-between gap-5"
-                role={"button"}
-                onClick={() => setOpened(true)}
-            >
-                <div className="title-and-date">
-                    <h3>
-                        {item?.entity_service?.title &&
-                        item?.entity_service?.title?.length > 40
-                            ? item?.entity_service?.title.substring(0, 40) +
-                              "..."
-                            : item?.entity_service?.title}
-                        {Approvedtask?.title && Approvedtask?.title?.length > 40
-                            ? Approvedtask?.title.substring(0, 40) + "..."
-                            : Approvedtask?.title}
-                    </h3>
-                    <div className="image-and-date d-flex align-items-center">
-                        {item?.entity_service?.created_by && (
-                            <Image
-                                src={
-                                    item?.entity_service?.created_by
-                                        ?.profile_image
-                                        ? item?.entity_service?.created_by
-                                              ?.profile_image
-                                        : "/placeholder/profilePlaceholder.png"
-                                }
-                                alt="circle image"
-                                height={25}
-                                width={25}
-                                objectFit="cover"
-                                className="profile-image"
-                            />
-                        )}
-                        {Approvedtask?.assignee && (
-                            <Image
-                                src={
-                                    Approvedtask?.assigner?.profile_image
-                                        ? Approvedtask?.assigner?.profile_image
-                                        : "/placeholder/profilePlaceholder.png"
-                                }
-                                alt="circle image"
-                                height={25}
-                                width={25}
-                                objectFit="cover"
-                                className="profile-image"
-                            />
-                        )}
-
-                        <span>
-                            {item?.entity_service?.created_by?.first_name}{" "}
-                            {item?.entity_service?.created_by?.middle_name}{" "}
-                            {item?.entity_service?.created_by?.last_name}
-                            {Approvedtask?.assigner?.first_name}{" "}
-                            {Approvedtask?.assigner?.middle_name}{" "}
-                            {Approvedtask?.assigner?.last_name}
-                        </span>
-                        <FontAwesomeIcon
-                            icon={faPeriod}
-                            className={"svg-icon"}
-                        />
-                        <span>
-                            {item?.created_at &&
-                                format(new Date(item?.created_at), "PP")}
-                            {Approvedtask?.created_at &&
-                                format(
-                                    new Date(Approvedtask?.created_at),
-                                    "PP"
-                                )}
-                        </span>
-                    </div>
-                </div>
+        <>
+            <div className="my-booking-task-card">
                 <div
-                    className="price d-flex flex-column align-items-end"
+                    className="title-price-wrapper d-flex justify-content-between gap-5"
+                    role={"button"}
                     onClick={() => setOpened(true)}
                 >
-                    <h2 className="text-nowrap">
-                        {item?.entity_service?.currency?.symbol} {""}
-                        {item?.budget_to}
-                        {Approvedtask?.currency?.symbol} {""}
-                        {Approvedtask?.charge}
-                    </h2>
-                    <p>
-                        {item?.entity_service?.budget_type}{" "}
-                        {Approvedtask?.entity_service?.budget_type}
-                    </p>
-                </div>
-            </div>
+                    <div className="title-and-date">
+                        <h3>
+                            {item?.entity_service?.title &&
+                            item?.entity_service?.title?.length > 40
+                                ? item?.entity_service?.title.substring(0, 40) +
+                                  "..."
+                                : item?.entity_service?.title}
+                            {Approvedtask?.title &&
+                            Approvedtask?.title?.length > 40
+                                ? Approvedtask?.title.substring(0, 40) + "..."
+                                : Approvedtask?.title}
+                        </h3>
+                        <div className="image-and-date d-flex align-items-center">
+                            {item?.entity_service?.created_by && (
+                                <Image
+                                    src={
+                                        item?.entity_service?.created_by
+                                            ?.profile_image
+                                            ? item?.entity_service?.created_by
+                                                  ?.profile_image
+                                            : "/placeholder/profilePlaceholder.png"
+                                    }
+                                    alt="circle image"
+                                    height={25}
+                                    width={25}
+                                    objectFit="cover"
+                                    className="profile-image"
+                                />
+                            )}
+                            {Approvedtask?.assignee && (
+                                <Image
+                                    src={
+                                        Approvedtask?.assigner?.profile_image
+                                            ? Approvedtask?.assigner
+                                                  ?.profile_image
+                                            : "/placeholder/profilePlaceholder.png"
+                                    }
+                                    alt="circle image"
+                                    height={25}
+                                    width={25}
+                                    objectFit="cover"
+                                    className="profile-image"
+                                />
+                            )}
 
-            <div
-                className="center-section d-flex align-items-center justify-content-between"
-                role={"button"}
-                onClick={() => setOpened(true)}
-            >
-                <div className="name-and-location">
-                    <div className="location d-flex align-items-center">
-                        <FontAwesomeIcon
-                            icon={faLocationDot}
-                            className="svg-icon"
-                        />
-                        <span>
-                            {item?.location} {Approvedtask?.location}
-                        </span>
+                            <span>
+                                {item?.entity_service?.created_by?.first_name}{" "}
+                                {item?.entity_service?.created_by?.middle_name}{" "}
+                                {item?.entity_service?.created_by?.last_name}
+                                {Approvedtask?.assigner?.first_name}{" "}
+                                {Approvedtask?.assigner?.middle_name}{" "}
+                                {Approvedtask?.assigner?.last_name}
+                            </span>
+                            <FontAwesomeIcon
+                                icon={faPeriod}
+                                className={"svg-icon"}
+                            />
+                            <span>
+                                {item?.created_at &&
+                                    format(new Date(item?.created_at), "PP")}
+                                {Approvedtask?.created_at &&
+                                    format(
+                                        new Date(Approvedtask?.created_at),
+                                        "PP"
+                                    )}
+                            </span>
+                        </div>
                     </div>
-                    <div className="location d-flex align-items-center">
-                        <FontAwesomeIcon
-                            icon={faHourglassClock}
-                            className="svg-icon"
-                        />
-                        {item?.start_date &&
-                            format(new Date(item?.start_date), "PP")}
-                        {Approvedtask?.start_date &&
-                            format(new Date(Approvedtask?.start_date), "PP")}
+                    <div
+                        className="price d-flex flex-column align-items-end"
+                        onClick={() => setOpened(true)}
+                    >
+                        <h2 className="text-nowrap">
+                            {item?.entity_service?.currency?.symbol} {""}
+                            {item?.budget_to}
+                            {Approvedtask?.currency?.symbol} {""}
+                            {Approvedtask?.charge}
+                        </h2>
+                        <p>
+                            {item?.entity_service?.budget_type}{" "}
+                            {Approvedtask?.entity_service?.budget_type}
+                        </p>
                     </div>
-                    {/* <div className="location">
-                        <FontAwesomeIcon
-                            icon={faUserGroup}
-                            className="svg-icon"
-                        />
-                        <span>100 Applied</span>
-                    </div> */}
                 </div>
-                {Approvedtask && (
-                    <RingProgress
-                        sections={[{ value: progress, color: color }]}
-                        thickness={9}
-                        roundCaps
-                        label={
-                            <Text
-                                color={color}
-                                weight={600}
-                                align="center"
-                                size="xl"
-                            >
-                                {progress}%
-                            </Text>
-                        }
-                    />
-                )}
-            </div>
 
-            <div className="d-flex justify-content-between align-items-center card-footer-section ">
-                <Badge color={color}>
-                    {item?.status} {Approvedtask?.status}
-                </Badge>
-                <div className="icons-section d-flex">
-                    <div className="share-icon">
-                        <ShareIcon url={""} quote={""} hashtag={""} showText />
+                <div
+                    className="center-section d-flex align-items-center justify-content-between"
+                    role={"button"}
+                    onClick={() => setOpened(true)}
+                >
+                    <div className="name-and-location">
+                        <div className="location d-flex align-items-center">
+                            <FontAwesomeIcon
+                                icon={faLocationDot}
+                                className="svg-icon"
+                            />
+                            <span>
+                                {item?.location} {Approvedtask?.location}
+                            </span>
+                        </div>
+                        <div className="location d-flex align-items-center">
+                            <FontAwesomeIcon
+                                icon={faHourglassClock}
+                                className="svg-icon"
+                            />
+                            {item?.start_date &&
+                                format(new Date(item?.start_date), "PP")}
+                            {Approvedtask?.start_date &&
+                                format(
+                                    new Date(Approvedtask?.start_date),
+                                    "PP"
+                                )}
+                        </div>
+                        {/* <div className="location">
+        <FontAwesomeIcon
+            icon={faUserGroup}
+            className="svg-icon"
+        />
+        <span>100 Applied</span>
+    </div> */}
+                    </div>
+                    {Approvedtask && (
+                        <RingProgress
+                            sections={[{ value: progress, color: color }]}
+                            thickness={9}
+                            roundCaps
+                            label={
+                                <Text
+                                    color={color}
+                                    weight={600}
+                                    align="center"
+                                    size="xl"
+                                >
+                                    {progress}%
+                                </Text>
+                            }
+                        />
+                    )}
+                </div>
+
+                <div className="d-flex justify-content-between align-items-center card-footer-section ">
+                    <Badge color={color}>
+                        {item?.status} {Approvedtask?.status}
+                    </Badge>
+                    <div className="icons-section d-flex">
+                        <div className="share-icon">
+                            <ShareIcon
+                                url={""}
+                                quote={""}
+                                hashtag={""}
+                                showText
+                            />
+                        </div>
+                        {status === "Open" && (
+                            <span className="ordered-date">
+                                <Button
+                                    variant="light"
+                                    color={"orange"}
+                                    onClick={() => {
+                                        taskCompleteHandler(
+                                            Approvedtask?.id
+                                                ? Approvedtask?.id
+                                                : ""
+                                        );
+                                    }}
+                                    loading={taskCompleteMutaion.isLoading}
+                                >
+                                    Completed
+                                </Button>
+                            </span>
+                        )}
                     </div>
                 </div>
+                <BookingDetails
+                    show={opened}
+                    setShow={setOpened}
+                    bookingId={String(item?.id) ?? ""}
+                />
             </div>
-            <BookingDetails
-                show={opened}
-                setShow={setOpened}
-                bookingId={String(item?.id) ?? ""}
-            />
-        </div>
+            {Approvedtask?.id && (
+                <ReviewModal
+                    open={
+                        (status === "Completed" || "closed") && openReviewModal
+                    }
+                    handleClose={() => setOpenReviewModal(false)}
+                    taskId={Approvedtask?.id}
+                />
+            )}
+        </>
     );
 };
