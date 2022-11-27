4b8d219e5cfe770e9db94624e06c7bfeed98e255
Sandip <sandipthapa@cagtu.com>
2022-11-27 17:23:18 +0545

    Date shown for all activities

--- components/Profile/Activities.tsx
+++ components/Profile/Activities.tsx
@@ -1,13 +1,10 @@
 import { ScrollArea } from "@mantine/core";
 import { format } from "date-fns";
-import { useData } from "hooks/use-data";
 import { useInViewPort } from "hooks/use-in-viewport";
 import { useUserActivities } from "hooks/userActivities/use-userActivities";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import type { UserProfileProps } from "types/userProfileProps";
 
 const UserActivities = () => {
     const {
@@ -31,10 +28,10 @@ const UserActivities = () => {
         }
     });
 
-    const [id, setId] = useState<number | undefined>(activities?.result[0].id);
-    const [showFirstDate, setShowFirstDate] = useState(true);
+    // const [id, setId] = useState<number | undefined>(activities?.result[0].id);
+    // const [showFirstDate, setShowFirstDate] = useState(true);
 
-    let idFirst = activities?.result[0].id;
+    // let idFirst = activities?.result[0].id;
 
     return (
         <div className="activities">
@@ -48,43 +45,22 @@ const UserActivities = () => {
                         <div
                             className="timeline"
                             key={activity.id}
-                            onClick={() => {
-                                setId(activity.id);
-                                setShowFirstDate(false);
-                                idFirst = 0;
-                            }}
                             ref={isLastActivityOnPage(index) ? ref : null}
                         >
                             <Row className="w-100">
                                 <Col md={1} sm={2} xs={2}>
-                                    {id === activity.id ||
-                                    (idFirst === activity.id &&
-                                        showFirstDate) ? (
-                                        <div className="date">
-                                            <p>
-                                                {format(
-                                                    new Date(
-                                                        activity.action_time
-                                                    ),
-                                                    "dd MMM yyyy"
-                                                )}{" "}
-                                            </p>
-                                        </div>
-                                    ) : (
-                                        ""
-                                    )}
+                                    <div className="date">
+                                        <p>
+                                            {format(
+                                                new Date(activity.action_time),
+                                                "dd MMM yyyy"
+                                            )}{" "}
+                                        </p>
+                                    </div>
                                 </Col>
                                 <Col md={10} sm={9} xs={9}>
                                     <div className="content d-flex">
-                                        <div
-                                            className={
-                                                id === activity.id ||
-                                                (idFirst === activity.id &&
-                                                    showFirstDate)
-                                                    ? "point-active"
-                                                    : "point"
-                                            }
-                                        ></div>
+                                        <div></div>
                                         <div className="desc">
                                             {activity.action === "Login" && (
                                                 <figure className="thumbnail-img">
