aaf5ef741294977e93205dd4605c73e44bd87522
Sandip <sandipthapa@cagtu.com>
2022-12-11 15:49:19 +0545

    Optional Chainning added

--- utils/extractString.ts
+++ utils/extractString.ts
@@ -1,4 +1,4 @@
 export default function extractContent(s: any) {
-    const content_holder = s.replace(/<(?:.|\n)*?>/gm, "");
+    const content_holder = s?.replace(/<(?:.|\n)*?>/gm, "");
     return content_holder;
 }
