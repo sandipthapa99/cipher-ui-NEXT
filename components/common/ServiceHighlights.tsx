e4de09464f51a85962f523ecd256f01ba1097320
Samikshya <samikshyabasnet@cagtu.com>
2022-12-15 17:18:03 +0545

    didn't change highlight in a task detail

--- components/common/ServiceHighlights.tsx
+++ components/common/ServiceHighlights.tsx
@@ -6,15 +6,19 @@ interface ServiceHighlightsProps {
     highlights: string[];
 }
 const ServiceHighlights = ({ highlights = [] }: ServiceHighlightsProps) => {
+    console.log(
+        "🚀 ~ file: ServiceHighlights.tsx ~ line 9 ~ ServiceHighlights ~ highlights",
+        highlights
+    );
     return (
         <>
-            {highlights?.map((value: { id: string; name: string }, key) => (
+            {highlights?.map((value, key) => (
                 <p className="mb-4" key={key}>
                     <FontAwesomeIcon
                         icon={faCheck}
                         className="me-3 svg-icon svg-icon-check"
                     />
-                    {value.name}
+                    {value}
                 </p>
             ))}
         </>
