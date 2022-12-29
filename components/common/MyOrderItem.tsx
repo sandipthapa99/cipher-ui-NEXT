b62905ba9b0a6d3bc0b87d854acf6b9187833148
Sandip Thapa <sandipthapa@cagtu.com>
2022-12-29 14:35:05 +0545

    Removed redirection from my order tab

--- components/common/MyOrderItem.tsx
+++ components/common/MyOrderItem.tsx
@@ -17,84 +17,74 @@ export const MyOrderItem = ({
                         <div className="order-detail-section border-bottom-0 bg-white">
                             <Row>
                                 <Col lg={2} md={6} sm={6} xs={12}>
-                                    <Link href={`/task/${item?.task?.id}`}>
-                                        <a>
-                                            <figure className="d-flex align-items-center justify-content-start h-100 w-100 order-detail-section__image">
-                                                {item?.task
-                                                    ?.entity_service_images
-                                                    .length > 0 ? (
-                                                    <Image
-                                                        src={
-                                                            item?.task
-                                                                ?.entity_service_images[0]
-                                                                .media
-                                                        }
-                                                        alt="order-detail-image"
-                                                        height={160}
-                                                        width={180}
-                                                    />
-                                                ) : (
-                                                    <Image
-                                                        src={
-                                                            "/placeholder/taskPlaceholder.png"
-                                                        }
-                                                        alt="order-detail-image"
-                                                        objectFit="contain"
-                                                        height={160}
-                                                        width={140}
-                                                    />
-                                                )}
-                                            </figure>
-                                        </a>
-                                    </Link>
+                                    {/* <Link href={`/task/${item?.task?.id}`}>
+                                        <a> */}
+                                    <figure className="d-flex align-items-center justify-content-start h-100 w-100 order-detail-section__image">
+                                        {item?.task?.entity_service_images
+                                            .length > 0 ? (
+                                            <Image
+                                                src={
+                                                    item?.task
+                                                        ?.entity_service_images[0]
+                                                        .media
+                                                }
+                                                alt="order-detail-image"
+                                                height={160}
+                                                width={180}
+                                            />
+                                        ) : (
+                                            <Image
+                                                src={
+                                                    "/placeholder/taskPlaceholder.png"
+                                                }
+                                                alt="order-detail-image"
+                                                objectFit="contain"
+                                                height={160}
+                                                width={140}
+                                            />
+                                        )}
+                                    </figure>
+                                    {/* </a>
+                                    </Link> */}
                                 </Col>
                                 <Col md={6} sm={12}>
-                                    <Link href={`/task/${item?.task?.id}`}>
-                                        <a>
-                                            <div className="title-and-description text-black-50">
-                                                <h4>{item?.task?.title}</h4>
-                                                <h4>
-                                                    By{" "}
-                                                    {
-                                                        item?.task?.assignee
-                                                            .first_name
-                                                    }{" "}
-                                                    {item?.task?.assignee
-                                                        .middle_name ?? ""}{" "}
-                                                    {
-                                                        item?.task?.assignee
-                                                            .last_name
-                                                    }
-                                                </h4>
-                                                <div className="price-section">
-                                                    <span className="price">
-                                                        Price :{" "}
-                                                    </span>
-                                                    <span className="value">
-                                                        {item?.task?.currency}{" "}
-                                                        {item?.amount}
-                                                        {item?.task
-                                                            ?.budget_type ===
-                                                        "Hourly"
-                                                            ? "/hr"
-                                                            : item?.task
-                                                                  ?.budget_type ===
-                                                              "Monthly"
-                                                            ? "/mn"
-                                                            : item?.task
-                                                                  ?.budget_type ===
-                                                              "Daily"
-                                                            ? "/daily"
-                                                            : "/project"}
-                                                    </span>
-                                                </div>
-                                                <span className="item-id p-3 rounded">
-                                                    Item ID: #
-                                                    {item?.id.slice(0, 8)}
-                                                </span>
-                                            </div>
-                                        </a>
-                                    </Link>
+                                    {/* <Link href={`/task/${item?.task?.id}`}>
+                                        <a> */}
+                                    <div className="title-and-description text-black-50">
+                                        <h4>{item?.task?.title}</h4>
+                                        <h4>
+                                            By {item?.task?.assignee.first_name}{" "}
+                                            {item?.task?.assignee.middle_name ??
+                                                ""}{" "}
+                                            {item?.task?.assignee.last_name}
+                                        </h4>
+                                        <div className="price-section">
+                                            <span className="price">
+                                                Price :{" "}
+                                            </span>
+                                            <span className="value">
+                                                {item?.task?.currency}{" "}
+                                                {item?.amount}
+                                                {item?.task?.budget_type ===
+                                                "Hourly"
+                                                    ? "/hr"
+                                                    : item?.task
+                                                          ?.budget_type ===
+                                                      "Monthly"
+                                                    ? "/mn"
+                                                    : item?.task
+                                                          ?.budget_type ===
+                                                      "Daily"
+                                                    ? "/daily"
+                                                    : "/project"}
+                                            </span>
+                                        </div>
+                                        <span className="item-id p-3 rounded">
+                                            Item ID: #{item?.id.slice(0, 8)}
+                                        </span>
+                                    </div>
+                                    {/* </a>
+                                    </Link> */}
                                 </Col>
 
                                 {/* <Col md={12} lg={4}>
