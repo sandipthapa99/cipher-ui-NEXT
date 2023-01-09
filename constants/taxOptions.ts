174f599ae4bf6ba9a7127872e79b6baf591af7c3
darshanman <darshanman@cagtu.com>
2023-01-09 11:48:58 +0545

    options removed from utils to constants

---
+++ constants/taxOptions.ts
@@ -0,0 +1,5 @@
+export const maritalStatus = [{ id: 1, value: "Married", label: "Married" }];
+
+export const gender = [{ id: 1, value: "Male", label: "Male" }];
+
+export const salaryType = [{ id: 1, value: "Monthly", label: "Monthly" }];
