764e21d83cb40764f4c274482b79b12591af1dbe
Sandip <sandipthapa@cagtu.com>
2023-01-12 15:27:22 +0545

    Checkout page offers and promocode styles updated

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -444,7 +444,7 @@ export default function Checkout() {
                                             <>
                                                 {!item?.offer && offer && (
                                                     <p
-                                                        className="text-primary m-2"
+                                                        className="text-primary mb-0"
                                                         role={"button"}
                                                         onClick={() => {
                                                             setOffer(false);
@@ -556,7 +556,7 @@ export default function Checkout() {
                                                 )}
                                                 {!item?.offer && !offer && (
                                                     <p
-                                                        className="text-primary m-2"
+                                                        className="text-primary mt -1 mb-0"
                                                         role={"button"}
                                                         onClick={() => {
                                                             setOffer(true);
@@ -638,7 +638,7 @@ export default function Checkout() {
                                                             isSubmitting,
                                                             setFieldValue,
                                                         }) => (
-                                                            <Form className="d-flex justify-content-between gap-4 w-100 mt-4 mb-4">
+                                                            <Form className="d-flex justify-content-between gap-4 w-100 mt-2 mb-2">
                                                                 <Select
                                                                     placeholder="pick an offer"
                                                                     name="redeem_offer"
@@ -747,7 +747,7 @@ export default function Checkout() {
                             </p>
                         </div>
                         <Button
-                            className="checkout-btn"
+                            className="checkout-btn mt-5"
                             disabled={
                                 paymentType === "" ||
                                 errorMsg !== "" ||
